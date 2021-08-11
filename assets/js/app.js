0a30aa5b2177f212e094acf66c8c943868b9f3b0
Hannah Purcell <hpurcell@mbta.com>
2021-08-11 15:00:07 -0400

    Add TS and React

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+const _css = require("../css/app.scss");
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -13,3 +13,11 @@ import "../css/app.scss"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Greeter from "./greeter";
+
+const greeting = document.getElementById("greeting");
+ReactDOM.render(<Greeter name="Phoenix" />, greeting);
