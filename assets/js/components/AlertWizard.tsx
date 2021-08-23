94abb01e66b9c8c8f61858ac836c97b6aba1cc57
Hannah Purcell <hpurcell@mbta.com>
2021-08-23 15:27:28 -0400

    Basic app layout with header and IntroBlock

---
+++ assets/js/components/AlertWizard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SimpleForm from './SimpleForm';
+
+interface AlertWizardProps {}
+
+const AlertWizard = (props: AlertWizardProps): JSX.Element => {
+  return (
+    <SimpleForm />
+  )
+};
+
+export default AlertWizard;
---
+++ assets/js/components/IntroBlock.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { ExclamationIcon, PlusCircleIcon } from '@heroicons/react/solid';
+
+interface IntroBlockProps {
+  startAlertWizard: () => void
+}
+
+const IntroBlock = (props: IntroBlockProps): JSX.Element => {
+  return (
+    <div className="intro-block">
+      <div className="exclamation-circle">
+        <ExclamationIcon className="exclamation"/>
+      </div>
+      <ul className="intro-text">
+        <li>
+          This tool temporarily replaces Outfront Media advertising content in stations with rider-facing alerts.
+        </li>
+        <li>
+          Use it to reinforce Live PA audio during <span className="emphasized">safety-critical emergencies requiring riders to take action</span>.
+        </li>
+      </ul>
+      <button className="create-alert-button" onClick={props.startAlertWizard}>
+        <PlusCircleIcon className="plus-circle"/>
+        <span className="text-16">Create new Takeover Alert</span>
+      </button>
+    </div>
+  )
+};
+
+export default IntroBlock;
