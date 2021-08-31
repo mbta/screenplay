b4028c70beef8e56d17bbb97d70e6aad567f22be
Paul Handorff <handorff.paul@gmail.com>
2021-08-31 11:09:54 -0400

    UserActionLogger module

---
+++ lib/screenplay_web/user_action_logger.ex
@@ -0,0 +1,22 @@
+defmodule ScreenplayWeb.UserActionLogger do
+  @moduledoc """
+  Helper for logging ever action a user takes.
+  """
+
+  require Logger
+
+  @spec log(String.t(), atom()) :: :ok
+  @spec log(String.t(), atom(), map()) :: :ok
+  def log(username, action, params \\ %{}) do
+    params = Map.merge(%{username: username, action: action}, params)
+
+    Logger.info(["User action: ", params_iodata(params)])
+  end
+
+  @spec params_iodata(map()) :: [iodata()]
+  defp params_iodata(params) do
+    params
+    |> Enum.map(fn {key, value} -> [to_string(key), ?=, inspect(value)] end)
+    |> Enum.intersperse(", ")
+  end
+end
