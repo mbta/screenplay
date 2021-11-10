a3ac88902461bbe293d9d51165ce11844401259e
Maddox <cmaddox@CTD24876CMADDOX.local>
2021-11-10 08:44:53 -0500

    Added a Clear All button that only shows when at least 2 alerts exist.

--- assets/js/components/AlertDashboard/AlertsList.tsx
+++ assets/js/components/AlertDashboard/AlertsList.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import AlertDetails from "./AlertDetails";
 import { AlertData } from "../App";
 import { ModalDetails } from "../ConfirmationModal";
+import { BanIcon } from "@heroicons/react/solid";
 
 interface AlertsListProps {
   startEditWizard: (data: AlertData) => void;
@@ -61,11 +62,75 @@ const AlertsList = (props: AlertsListProps): JSX.Element => {
     props.closeModal();
   };
 
+  const clearAllAlerts = () => {
+    const csrfMetaElement = document.head.querySelector(
+      "[name~=csrf-token][content]"
+    ) as HTMLMetaElement;
+    const csrfToken = csrfMetaElement.content;
+
+    fetch("/api/clear_all", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-csrf-token": csrfToken,
+      },
+      credentials: "include",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+
+        return response.json();
+      })
+      .then(({ success }) => {
+        if (success) {
+          setLastChangeTime(Date.now());
+        } else {
+          // Should this be a toast or other user-visible message?
+          console.log("Error when clearing all alerts");
+        }
+      })
+      .catch((error) => {
+        // Should this be a toast or other user-visible message?
+        console.log("Failed to clear all alerts: ", error);
+      });
+
+    props.closeModal();
+  };
+
+  const clearAllModalDetails: ModalDetails = {
+    icon: <BanIcon className="icon" />,
+    header: "Clear all Alerts",
+    description:
+      "This stops all Outfront Media screen Takeovers, and returns to the regularly scheduled content loop.",
+    cancelText: "Keep Alerts",
+    confirmJSX: (
+      <>
+        <BanIcon className="button-icon" />
+        Clear all Alerts
+      </>
+    ),
+    onSubmit: () => clearAllAlerts(),
+  };
+
   return (
     <>
       {alertsData.length > 0 ? (
         <>
-          <div className="text-30 alerts-list-header">Live Takeover Alerts</div>
+          <div className="text-30 alerts-list-header">
+            <span>Live Takeover Alerts</span>
+            {alertsData.length > 1 && (
+              <button
+                className="clear-all-alerts-button"
+                onClick={() => props.triggerConfirmation(clearAllModalDetails)}
+              >
+                <BanIcon className="button-icon" />
+                <span className="text-16">Clear all</span>
+              </button>
+            )}
+          </div>
+
           {alertsData.map((data) => {
             const { id } = data;
             return (
