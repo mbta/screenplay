aacd3866eca5cb719095e5d1ee75794152f98925
Brian Fauble <bfauble@users.noreply.github.com>
2021-11-12 08:31:50 -0600

    feat: alert reminders (#62)
    * update reminder timestamp on alert edit
    
    * force periodic list refresh to show reminder prompts for alerts that are still active beyond reminder time
    
    * style alert reminder banner

---
+++ assets/js/components/AlertDashboard/AlertReminder.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ClockIcon } from "@heroicons/react/solid";
+
+const AlertReminder = ({ clearAlert, editAlert, endDate }): JSX.Element => {
+  if (!endDate) return null
+  const endDateObj = new Date(endDate)
+  if (new Date() < endDateObj) return null
+
+  return (
+    <div className="alert-reminder">
+      <ClockIcon className="reminder-icon" />
+      <div>
+        <span>Reminder that this alert is still posted. Please </span>
+        <span className="reminder-action" onClick={editAlert}>Edit</span> 
+        <span> for a later reminder, or </span>
+        <span className="reminder-action" onClick={clearAlert}>Clear</span> 
+        <span> this Alert</span> 
+      </div>
+    </div>
+  )
+}
+
+export default AlertReminder;
