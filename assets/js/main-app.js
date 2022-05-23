5b5c24ccb9913b21a13967e0a577f08e1e8d875b
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-05-23 09:57:59 -0400

    Too many routers.

--- assets/js/main-app.js
+++ assets/js/main-app.js
@@ -20,9 +20,4 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./components/App";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("app")
-);
+ReactDOM.render(<App />, document.getElementById("app"));
