e83e3098fc144d3cfb2f462c1461f762d4946d20
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-05-26 11:23:24 -0400

    Removed type declaration.

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -1,7 +1,6 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-declare function require(name: string): string;
 require("../css/app.scss");
 
 // webpack automatically bundles all modules in your
