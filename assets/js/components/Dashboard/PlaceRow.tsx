209ab712643f8ef9aa7c3bc076e75bd581ca3942
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-05-31 16:04:31 -0400

    Added some logic for places with no screens.

--- assets/js/components/Dashboard/PlaceRow.tsx
+++ assets/js/components/Dashboard/PlaceRow.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import {
   Accordion,
-  Card,
   Col,
   Container,
   Row,
@@ -22,14 +21,18 @@ interface ModalProps {
 
 const PlaceRow = (props: ModalProps): JSX.Element => {
   const { activeEventKey } = useContext(AccordionContext);
-  const formatScreenTypes = () => {
+  const rowOnClick = useAccordionButton(props.eventKey);
+  const isOpen = activeEventKey === props.eventKey;
+  const hasScreens = props.screenTypes.length !== 0;
+
+  function formatScreenTypes() {
     if (props.screenTypes.length === 0) {
       return "no screens";
     }
     return props.screenTypes.join(" · ");
-  };
+  }
 
-  const renderModesAndLinesIcons = () => {
+  function renderModesAndLinesIcons() {
     return props.modesAndLines.map((modeOrLine) => (
       <img
         className="place-mode-line-icon"
@@ -40,38 +43,42 @@ const PlaceRow = (props: ModalProps): JSX.Element => {
         height={20}
       />
     ));
-  };
+  }
 
-  const rowOnClick = useAccordionButton(props.eventKey);
-  const isOpen = useContext(AccordionContext).activeEventKey === props.eventKey;
   return (
     <div
+      key={props.eventKey}
       className={classNames("place-row", {
         open: isOpen,
+        disabled: !hasScreens,
       })}
     >
       <Container fluid>
-        <Row onClick={rowOnClick} className="align-items-center text-white">
-          <Col lg="auto">
-            {activeEventKey === props.eventKey ? (
-              <ChevronDown size={16} />
-            ) : (
-              <ChevronRight size={16} />
-            )}
+        <Row
+          onClick={hasScreens ? rowOnClick : () => undefined}
+          className="align-items-center text-white"
+        >
+          <Col
+            lg="auto"
+            className={classNames({
+              "hidden-toggle": !hasScreens,
+            })}
+          >
+            {isOpen ? <ChevronDown size={16} /> : <ChevronRight size={16} />}
           </Col>
-          <Col lg={4} className="place-name">
+          <Col lg={2} className="place-name">
             {props.name}
           </Col>
-          <Col lg="auto" className="pe-5">
+          <Col lg={2} className="pe-5 d-flex justify-content-end">
             {renderModesAndLinesIcons()}
           </Col>
-          <Col lg="auto" className="place-screen-types pe-5">
+          <Col lg={2} className="place-screen-types pe-5">
             {formatScreenTypes()}
           </Col>
-          <Col lg="auto" className="place-stop-id">
+          <Col lg={1} className="place-stop-id">
             {props.stopId}
           </Col>
-          <Col lg={3} className="text-center pe-3">
+          <Col lg={2} className="text-center pe-3">
             {props.status}
           </Col>
         </Row>
