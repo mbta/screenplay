f3f188d9acf8069adbc49df98a4a884f85882d0f
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-06-02 09:03:49 -0400

    Formatting.

--- assets/js/models/screen.ts
+++ assets/js/models/screen.ts
@@ -1,7 +1,7 @@
 export interface Screen {
-    id: string;
-    stationCode?: string;
-    type: string;
-    zone?: string;
-    disabled: boolean;
+  id: string;
+  stationCode?: string;
+  type: string;
+  zone?: string;
+  disabled: boolean;
 }
