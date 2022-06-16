39efa7a46b12dada97d988317a5831aaa9194557
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-06-16 14:38:29 -0400

    Added a custom dropdown menu to help with matching designs.

--- assets/js/components/Dashboard/FilterDropdown.tsx
+++ assets/js/components/Dashboard/FilterDropdown.tsx
@@ -12,6 +12,29 @@ interface FilterDropdownProps {
   selectedValue: FilterDropdownItem;
 }
 
+interface CustomMenuProps {
+  children?: React.ReactNode;
+  style: any;
+  className: any;
+  "aria-labelledby": any;
+}
+
+const CustomMenu = React.forwardRef<HTMLElement, CustomMenuProps>(
+  ({ children, style, className, "aria-labelledby": labeledBy }, ref) => {
+    console.log(children);
+    return (
+      <div
+        ref={ref as any}
+        style={style}
+        className={className}
+        aria-labelledby={labeledBy}
+      >
+        <ul>{children}</ul>
+      </div>
+    );
+  }
+);
+
 /**
  * Component used to display each place and their screen simulations.
  * Assumes it is displayed in an Accordion component from react-bootstrap.
@@ -27,23 +50,31 @@ const FilterDropdown = (props: FilterDropdownProps): JSX.Element => {
         <Button
           onClick={() => onSelect(list[0].label)}
           data-testid="filter-dropdown-clear-button"
+          className="filter-dropdown__clear-button"
         >
           <XCircleFill size={16} className="m-0" />
         </Button>
       )}
       <Dropdown onSelect={onSelect} as={ButtonGroup}>
         <Dropdown.Toggle
-          className="filter-dropdown__dropdown-button"
+          className="filter-dropdown__dropdown-button d-flex justify-content-between"
           title={selectedValue.label}
-          size="lg"
           data-testid="filter-dropdown-button"
         >
           {selectedValue.label}
         </Dropdown.Toggle>
-        <Dropdown.Menu>
+        <Dropdown.Menu
+          as={CustomMenu}
+          className="filter-dropdown__dropdown-menu"
+        >
           {list.map(({ label }) => {
             return (
-              <Dropdown.Item key={label} eventKey={label}>
+              <Dropdown.Item
+                key={label}
+                eventKey={label}
+                active={label === selectedValue.label}
+                className="filter-dropdown__dropdown-item"
+              >
                 {label}
               </Dropdown.Item>
             );
