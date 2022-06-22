5e19fd26da7551d7b09428388823455f12f96ec2
Christian Maddox <christian.maddox93@gmail.com>
2022-06-22 08:02:50 -0400

    feat: Place List filter/sorting controls (#88)
    * Removed config from git tracking.
    
    * Added places_and_screens to gitignore.
    
    * Added an endpoint for fetching data. Only local fetching right now.
    
    * Added a config fetch module for S3.
    
    * Modified script to format Bus/CR routes as Bus/CR.
    
    * Added route sorting to script.
    
    * Added default place sorting to script.
    
    * Added a test file.
    
    * Credo.
    
    * Added a sobelow skip.
    
    * Added data fetching to frontend.
    
    * Moved dedup so bus doesn't show up more than once.
    
    * Changed property used for key.
    
    * Added a map for screen types.
    
    * Adjusted column width.
    
    * Changed status to be hardcoded until we use it.
    
    * Removed stop_id column.
    
    * Fixed tests.
    
    * Added config_fetcher module to test.
    
    * Added jest-dom for testing against the DOM.
    
    * Changed icons to show GL if more than one branch or branch if only one branch.
    
    * Prettier.
    
    * Removed a console log.
    
    * Added place sorting to frontend.
    
    * Added a component for filter dropdowns.
    
    * Added headers to table of places.
    
    * Added a color we will use a lot.
    
    * Removed a class that isn't doing anything.
    
    * Removed span. Unnecessary.
    
    * Changed prop name.
    
    * Changed X to the correct icon.
    
    * Added tests.
    
    * Changed dropdown list to be objects to help evaluate ids when filtering.
    
    * Added color and hover css.
    
    * Added a custom dropdown menu to help with matching designs.
    
    * Added a border-radius to dropdown menu.
    
    * Removed console log.
    
    * Added a transform so dropdown menu is centered under dropdown.
    
    * Added a class to reduce dropdown width if clear button is visible.
    
    * Centered sorting arrow.
    
    * Added a check next to selected filter item.
    
    * Removed unnecessary custom color.
    
    Co-authored-by: Maddox <cmaddox@CTD24876CMADDOX.local>

--- assets/js/components/Dashboard/Dashboard.tsx
+++ assets/js/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,63 @@
 import React, { useEffect, useState } from "react";
 import PlaceRow from "./PlaceRow";
+import FilterDropdown from "./FilterDropdown";
 import { Accordion, Container, Row, Col } from "react-bootstrap";
-import { Place } from "../../models/place";
+import { ArrowDown } from "react-bootstrap-icons";
 import "../../../css/screenplay.scss";
+import { Place } from "../../models/place";
+
+const modesAndLines = [
+  { label: "All MODES", ids: ["All"] },
+  { label: "Red Line", ids: ["Red"] },
+  { label: "Orange Line", ids: ["Orange"] },
+  { label: "Green Line B C D E", ids: ["Green"] },
+  { label: "Green Line B", ids: ["Green-B"] },
+  { label: "Green Line C", ids: ["Green-C"] },
+  { label: "Green Line D", ids: ["Green-D"] },
+  { label: "Green Line E", ids: ["Green-E"] },
+  { label: "Blue Line", ids: ["Blue"] },
+  { label: "Silver Line", ids: ["Silver"] },
+  { label: "Bus", ids: ["Bus"] },
+  { label: "Commuter Rail", ids: ["CR"] },
+  { label: "Ferry", ids: ["Ferry"] },
+  { label: "Access", ids: ["Access"] },
+];
+
+const screenTypes = [
+  { label: "All SCREEN TYPES", ids: ["All"] },
+  { label: "Bus Shelter", ids: ["bus_shelter_v2"] },
+  { label: "DUP", ids: ["dup"] },
+  { label: "E-Ink: Bus", ids: ["bus_eink", "bus_eink_v2"] },
+  {
+    label: "E-Ink: Green Line",
+    ids: ["gl_eink_single", "gl_eink_double", "gl_eink_v2"],
+  },
+  { label: "Elevator", ids: ["elevator"] },
+  { label: "Outfront Media", ids: ["ofm"] },
+  { label: "PA/ESS", ids: ["pa_ess"] },
+  { label: "Pre Fare Duo", ids: ["pre_fare_v2"] },
+  { label: "Solari", ids: ["solari"] },
+];
+
+const statuses = [
+  { label: "Any STATUS", ids: ["Any"] },
+  { label: "Auto", ids: ["Auto"] },
+  { label: "Headway mode", ids: ["Headway"] },
+  { label: "Overrides", ids: ["Overrides"] },
+  { label: "Emergency takeovers", ids: ["Takeovers"] },
+  { label: "Errors", ids: ["Errors"] },
+  { label: "Screen off", ids: ["Off"] },
+];
 
 const Dashboard = (): JSX.Element => {
   const [places, setPlaces] = useState([]);
+  const [modeLineFilterValue, setModeLineFilterValue] = useState(
+    modesAndLines[0]
+  );
+  const [screenTypeFilterValue, setScreenTypeFilterValue] = useState(
+    screenTypes[0]
+  );
+  const [statusFilterValue, setStatusFilterValue] = useState(statuses[0]);
 
   useEffect(() => {
     fetch("/api/dashboard")
@@ -17,23 +69,81 @@ const Dashboard = (): JSX.Element => {
       });
   }, []);
 
+  const handleModeOrLineSelect = (value: string) => {
+    const selectedFilter = modesAndLines.find(({ label }) => label === value);
+    if (selectedFilter) {
+      setModeLineFilterValue(selectedFilter);
+    }
+  };
+
+  const handleScreenTypeSelect = (value: string) => {
+    const selectedFilter = screenTypes.find(({ label }) => label === value);
+    if (selectedFilter) {
+      setScreenTypeFilterValue(selectedFilter);
+    }
+  };
+
+  const handleStatusSelect = (value: string) => {
+    const selectedFilter = statuses.find(({ label }) => label === value);
+    if (selectedFilter) {
+      setStatusFilterValue(selectedFilter);
+    }
+  };
+
   return (
     <Container fluid>
       <Row>
         <Col lg={1} style={{ background: "black" }}></Col>
         <Col
           lg={11}
-          style={{ background: "#171F26", padding: "24px 32px 32px 32px" }}
+          style={{
+            background: "#171F26",
+            padding: "24px 32px 32px 32px",
+          }}
         >
-          <Accordion flush alwaysOpen>
-            {places.map((place: Place, index) => (
-              <PlaceRow
-                key={place.id}
-                place={place}
-                eventKey={index.toString()}
-              />
-            ))}
-          </Accordion>
+          <Container fluid>
+            <Row className="place-list__header-row">
+              <Col
+                className="place-list__sort-label d-flex align-items-center"
+                lg={4}
+              >
+                ABC <ArrowDown />
+              </Col>
+              <Col
+                className="place-list__mode-line-filter d-flex justify-content-end pe-5"
+                lg={2}
+              >
+                <FilterDropdown
+                  list={modesAndLines}
+                  onSelect={(value: any) => handleModeOrLineSelect(value)}
+                  selectedValue={modeLineFilterValue}
+                />
+              </Col>
+              <Col className="place-list__screen-type-filter pe-5" lg={3}>
+                <FilterDropdown
+                  list={screenTypes}
+                  onSelect={(value: any) => handleScreenTypeSelect(value)}
+                  selectedValue={screenTypeFilterValue}
+                />
+              </Col>
+              <Col className="place-list__status-filter" lg={2}>
+                <FilterDropdown
+                  list={statuses}
+                  onSelect={(value: any) => handleStatusSelect(value)}
+                  selectedValue={statusFilterValue}
+                />
+              </Col>
+            </Row>
+            <Accordion flush alwaysOpen>
+              {places.map((place: Place, index) => (
+                <PlaceRow
+                  key={place.id}
+                  place={place}
+                  eventKey={index.toString()}
+                />
+              ))}
+            </Accordion>
+          </Container>
         </Col>
       </Row>
     </Container>
---
+++ assets/tests/components/filterDropdown.test.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import FilterDropdown from "../../js/components/Dashboard/FilterDropdown";
+
+describe("Dashboard", () => {
+  test("renders clear filter button if default is not selected", async () => {
+    const list = [
+      { label: "item1", ids: ["item1"] },
+      { label: "item2", ids: ["item2"] },
+      { label: "item3", ids: ["item3"] },
+    ];
+    const { getByTestId } = render(
+      <FilterDropdown
+        list={list}
+        onSelect={() => null}
+        selectedValue={list[1]}
+      />
+    );
+
+    expect(getByTestId("filter-dropdown-clear-button")).toBeVisible();
+  });
+
+  test("renders default if clear button is clicked", async () => {
+    const list = [
+      { label: "item1", ids: ["item1"] },
+      { label: "item2", ids: ["item2"] },
+      { label: "item3", ids: ["item3"] },
+    ];
+    const onSelect = (newValue: any) => {
+      const selectedItem = list.find(({ label }) => label === newValue);
+      if (selectedItem) {
+        selectedValue = selectedItem;
+      }
+    };
+    let selectedValue = list[1];
+    const { getByTestId, rerender, queryByTestId } = render(
+      <FilterDropdown
+        list={list}
+        onSelect={onSelect}
+        selectedValue={selectedValue}
+      />
+    );
+
+    expect(getByTestId("filter-dropdown-clear-button")).toBeVisible();
+    fireEvent.click(getByTestId("filter-dropdown-clear-button"));
+    rerender(
+      <FilterDropdown
+        list={list}
+        onSelect={onSelect}
+        selectedValue={selectedValue}
+      />
+    );
+    expect(queryByTestId("filter-dropdown-clear-button")).toBeNull();
+    expect(getByTestId("filter-dropdown-button").firstChild?.textContent).toBe(
+      list[0].label
+    );
+  });
+});
