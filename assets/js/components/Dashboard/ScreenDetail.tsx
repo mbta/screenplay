5ef1e91305913fec31003a977393ae5ed3c1dceb
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-06-30 08:55:51 -0400

    Added function to translate screen types into user-friendly labels.

--- assets/js/components/Dashboard/ScreenDetail.tsx
+++ assets/js/components/Dashboard/ScreenDetail.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Screen } from "../../models/screen";
 import ReportAProblemButton from "./ReportAProblemButton";
+import { SCREEN_TYPES } from "../../constants/constants";
 
 interface ScreenDetailProps {
   screen: Screen;
@@ -8,10 +9,15 @@ interface ScreenDetailProps {
 
 const ScreenDetail = (props: ScreenDetailProps): JSX.Element => {
   const { id, type } = props.screen;
+
+  const translateScreenType = () => {
+    return SCREEN_TYPES.find(({ ids }) => ids.includes(type))?.label;
+  };
+
   return (
     <div className="screen-detail__container">
       <div className="screen-detail__screen-type-location">
-        {type} / Location
+        {translateScreenType()} / Location
       </div>
       <div className="screen-detail__report-a-problem-button">
         <ReportAProblemButton id={id} />
