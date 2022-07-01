89aa8523053c7aeb7b00fd0ade26d4bf6d170635
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-07-01 11:12:50 -0400

    Changed button to navigate to Slack channel.

--- assets/js/components/Dashboard/ReportAProblemButton.tsx
+++ assets/js/components/Dashboard/ReportAProblemButton.tsx
@@ -3,14 +3,13 @@ import { Button } from "react-bootstrap";
 import { FlagFill } from "react-bootstrap-icons";
 
 const ReportAProblemButton = (): JSX.Element => {
-  const reportAProblem = (e: SyntheticEvent) => {
-    e.stopPropagation();
-    console.log(`Problem reported for screen`);
-  };
-
   return (
     <div className="report-a-problem-button">
-      <Button onClick={reportAProblem}>
+      <Button
+        href="https://mbta.slack.com/channels/screens"
+        onClick={(e: SyntheticEvent) => e.stopPropagation()}
+        target="_blank"
+      >
         <FlagFill /> Report a problem
       </Button>
     </div>
