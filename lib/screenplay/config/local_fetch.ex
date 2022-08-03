ba581e767ea58084101278ecaa98f9b303758799
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-08-03 09:53:36 -0400

    Added fetching of locations file. Will be used to give each screen a location to be displayed for the simulation.

--- lib/screenplay/config/local_fetch.ex
+++ lib/screenplay/config/local_fetch.ex
@@ -4,15 +4,19 @@ defmodule Screenplay.Config.LocalFetch do
   @behaviour Screenplay.Config.Fetch
 
   def get_config do
-    case do_get() do
-      {:ok, file_contents} -> Jason.decode(file_contents)
+    with {:ok, config_contents} <- do_get(:local_config_file_spec),
+         {:ok, location_contents} <- do_get(:local_locations_file_spec),
+         {:ok, config_json} <- Jason.decode(config_contents),
+         {:ok, location_json} <- Jason.decode(location_contents) do
+      {:ok, config_json, location_json}
+    else
       _ -> :error
     end
   end
 
   # sobelow_skip ["Traversal.FileModule"]
-  defp do_get do
-    path = local_config_path()
+  defp do_get(file_spec) do
+    path = local_path(file_spec)
 
     case File.read(path) do
       {:ok, contents} -> {:ok, contents}
@@ -20,8 +24,8 @@ defmodule Screenplay.Config.LocalFetch do
     end
   end
 
-  defp local_config_path do
-    case Application.get_env(:screenplay, :local_config_file_spec) do
+  defp local_path(file_spec) do
+    case Application.get_env(:screenplay, file_spec) do
       {:priv, file_name} -> Path.join(:code.priv_dir(:screenplay), file_name)
       {:test, file_name} -> Path.join(~w[#{File.cwd!()} test fixtures #{file_name}])
     end
