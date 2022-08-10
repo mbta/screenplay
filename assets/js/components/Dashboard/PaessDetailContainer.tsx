b5832be9ff19c34e51e35d1afbe79f051de10930
Paul Kim <pauljk704@gmail.com>
2022-08-10 09:16:52 -0400

    Screen Simulations - PAESS (#107)
    * Added a new component that will hold each screen simulation.
    
    * Added new component to each PlaceRow.
    
    * Moved button into its own component.
    
    * Moved constants to a constants file.
    
    * Added function to translate screen types into user-friendly labels.
    
    * Formatting.
    
    * Fixed test.
    
    * Added sort order for screens.
    
    * Changed screen detail blocks to group all pa_ess signs together.
    
    * Changed button to navigate to Slack channel.
    
    * Fixed test.
    
    * Added a conditional so the iframes won't load unless the accordion panel is open.
    
    * Added screen simulations for eink and lcd screens.
    
    * Cleaned up CSS a bit.
    
    * Adjusted styling for v2 screens now that scaling is done in screens code.
    
    * Added styling for v1 iframes.
    
    * Changed filters to clear activeKeys when a filter is changed.
    
    * Changed v2 simulation endpoint.
    
    * Changed script to allow for fetching data from different environments.
    
    * Added environment_name as an app data attribute.
    
    * Added a background color to normally transparent screens.
    
    * Changed baseUrl based on environment.
    
    * Fixed a margin.
    
    * Fixed screens URL for prod.
    
    * Fixed tests.
    
    * Add paess simulations to screenplay
    
    * Add grouping and styling of paess simulations based on route
    
    * Add some basic testing
    
    * always render each column
    
    * Add test for label
    
    * avoid usages of non null assertion
    
    * prettier
    
    * Update assets/js/components/Dashboard/PaessScreenDetail.tsx
    
    Co-authored-by: Christian Maddox <christian.maddox93@gmail.com>
    
    * Update assets/js/components/Dashboard/ScreenDetail.tsx
    
    Co-authored-by: Christian Maddox <christian.maddox93@gmail.com>
    
    * Update assets/js/components/Dashboard/ScreenDetail.tsx
    
    Co-authored-by: Christian Maddox <christian.maddox93@gmail.com>
    
    * Update assets/js/components/Dashboard/ScreenDetail.tsx
    
    Co-authored-by: Christian Maddox <christian.maddox93@gmail.com>
    
    * Address most comments
    
    * remove unneeded css
    
    * fix unit test
    
    * Don't forget Mattapan
    
    Co-authored-by: Maddox <cmaddox@CTD24876CMADDOX.local>
    Co-authored-by: Christian Maddox <christian.maddox93@gmail.com>

---
+++ assets/js/components/Dashboard/PaessDetailContainer.tsx
@@ -0,0 +1,103 @@
+import * as React from "react";
+import PaessScreenDetail from "./PaessScreenDetail";
+import { Screen } from "../../models/screen";
+import { Row, Col } from "react-bootstrap";
+
+interface PaessDetailContainerProps {
+  screens: Screen[];
+}
+
+const PaessDetailContainer = (
+  props: PaessDetailContainerProps
+): JSX.Element => {
+  const zonePositions = {
+    left: ["s", "w"],
+    center: ["c", "m"],
+    right: ["n", "e"],
+  };
+
+  const stationCode = props.screens[0].station_code;
+  const leftScreens = props.screens.filter(
+    (screen) => screen.zone != null && zonePositions.left.includes(screen.zone)
+  );
+  const centerScreens = props.screens.filter(
+    (screen) =>
+      screen.zone != null && zonePositions.center.includes(screen.zone)
+  );
+  const rightScreens = props.screens.filter(
+    (screen) => screen.zone != null && zonePositions.right.includes(screen.zone)
+  );
+
+  return (
+    <Row className="h-100" lg={3}>
+      <Col
+        key={`${stationCode}-left`}
+        className={`screen-detail__iframe-container screen-detail__iframe-container--pa_ess`}
+        data-testid="paess-col-left"
+      >
+        {leftScreens.length > 0 && (
+          <div>
+            {leftScreens.map(
+              (screen) =>
+                screen.station_code != null &&
+                screen.zone != null && (
+                  <PaessScreenDetail
+                    key={`${screen.station_code}-${screen.zone}`}
+                    stationCode={screen.station_code}
+                    zone={screen.zone}
+                    label={screen.label}
+                  />
+                )
+            )}
+          </div>
+        )}
+      </Col>
+      <Col
+        key={`${stationCode}-center`}
+        className={`screen-detail__iframe-container screen-detail__iframe-container--pa_ess`}
+        data-testid="paess-col-center"
+      >
+        {centerScreens.length > 0 && (
+          <div>
+            {centerScreens.map(
+              (screen) =>
+                screen.station_code != null &&
+                screen.zone != null && (
+                  <PaessScreenDetail
+                    key={`${screen.station_code}-${screen.zone}`}
+                    stationCode={screen.station_code}
+                    zone={screen.zone}
+                    label={screen.label}
+                  />
+                )
+            )}
+          </div>
+        )}
+      </Col>
+      <Col
+        key={`${stationCode}-right`}
+        className={`screen-detail__iframe-container screen-detail__iframe-container--pa_ess`}
+        data-testid="paess-col-right"
+      >
+        {rightScreens.length > 0 && (
+          <div>
+            {rightScreens.map(
+              (screen) =>
+                screen.station_code != null &&
+                screen.zone != null && (
+                  <PaessScreenDetail
+                    key={`${screen.station_code}-${screen.zone}`}
+                    stationCode={screen.station_code}
+                    zone={screen.zone}
+                    label={screen.label}
+                  />
+                )
+            )}
+          </div>
+        )}
+      </Col>
+    </Row>
+  );
+};
+
+export default PaessDetailContainer;
--- assets/js/components/Dashboard/ScreenDetail.tsx
+++ assets/js/components/Dashboard/ScreenDetail.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Screen } from "../../models/screen";
 import ReportAProblemButton from "./ReportAProblemButton";
 import { SCREEN_TYPES } from "../../constants/constants";
+import PaessDetailContainer from "./PaessDetailContainer";
+import classNames from "classnames";
 
 interface ScreenDetailProps {
   screens: Screen[];
@@ -14,8 +16,36 @@ const ScreenDetail = (props: ScreenDetailProps): JSX.Element => {
       ?.label;
   };
 
-  const getScreenLocation = () =>
-    props.screens[0].location ? `/ ${props.screens[0].location}` : "";
+  const isPaess = props.screens.every((screen) => screen.type === "pa_ess");
+
+  const getPaessRouteLetter = () => {
+    return props.screens[0].station_code
+      ? props.screens[0].station_code.charAt(0).toLowerCase()
+      : "";
+  };
+
+  const getPaessRoute = (routeLetter: string) => {
+    switch (routeLetter) {
+      case "g":
+        return "GREEN LINE";
+      case "r":
+        return "RED LINE";
+      case "m":
+        return "MATTAPAN";
+      case "b":
+        return "BLUE LINE";
+      case "o":
+        return "ORANGE LINE";
+      case "s":
+        return "BUS";
+    }
+  };
+
+  const getScreenLocation = () => {
+    if (isPaess) {
+      return `/ ${getPaessRoute(getPaessRouteLetter())}`;
+    } else props.screens[0].location ? `/ ${props.screens[0].location}` : "";
+  };
 
   const generateSource = (screen: Screen) => {
     const { id, type } = screen;
@@ -41,27 +71,47 @@ const ScreenDetail = (props: ScreenDetailProps): JSX.Element => {
   };
 
   return (
-    <div className="screen-detail__container">
+    <div
+      className={classNames("screen-detail__container", {
+        [`screen-detail__container--paess screen-detail__container--paess-${getPaessRouteLetter()}`]:
+          isPaess,
+      })}
+    >
       <div className="screen-detail__header">
-        <div className="screen-detail__screen-type-location">
+        <div
+          className={classNames("screen-detail__screen-type-location", {
+            "screen-detail__screen-type-location--paess-s":
+              isPaess && getPaessRouteLetter() == "s",
+            "screen-detail__screen-type-location--paess": isPaess,
+          })}
+        >
           {translateScreenType()} {getScreenLocation()}
         </div>
-        <div className="screen-detail__report-a-problem-button">
-          <ReportAProblemButton />
-        </div>
+        {!isPaess && (
+          <div className="screen-detail__report-a-problem-button">
+            <ReportAProblemButton />
+          </div>
+        )}
       </div>
       {props.isOpen &&
-        props.screens.map((screen) => (
-          <div
-            key={screen.id}
-            className={`screen-detail__iframe-container screen-detail__iframe-container--${screen.type}`}
-          >
-            <iframe
-              className={`screen-detail__iframe screen-detail__iframe--${screen.type}`}
-              title={screen.id}
-              src={generateSource(screen)}
-            />
-          </div>
+        (isPaess ? (
+          <PaessDetailContainer
+            key={props.screens[0].station_code}
+            screens={props.screens}
+          ></PaessDetailContainer>
+        ) : (
+          props.screens.map((screen) => (
+            <div
+              key={screen.id}
+              className={`screen-detail__iframe-container screen-detail__iframe-container--${screen.type}`}
+            >
+              <iframe
+                className={`screen-detail__iframe screen-detail__iframe--${screen.type}`}
+                title={screen.id}
+                src={generateSource(screen)}
+              />
+            </div>
+          ))
         ))}
     </div>
   );
