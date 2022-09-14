ba8a19d61148cc607b1f8842818a897e003ba4ed
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-09-14 09:11:44 -0400

    Added alerts controller for Screenplay Posted Alerts.

--- lib/screenplay_web/router.ex
+++ lib/screenplay_web/router.ex
@@ -54,7 +54,7 @@ defmodule ScreenplayWeb.Router do
     pipe_through [:redirect_prod_http, :browser, :auth, :ensure_auth]
 
     get("/dashboard", DashboardController, :index)
-    get("/dashboard/alerts", DashboardController, :index)
+    get("/alerts", AlertsController, :index)
     get("/unauthorized", UnauthorizedController, :index)
   end
 
