7ed72d9101aa43b7b18bd66a222196cc27762645
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-09-15 11:24:18 -0400

    Added a function to format alert effect.

---
+++ assets/js/components/Dashboard/AlertCard.tsx
@@ -0,0 +1,113 @@
+import moment from "moment";
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ActivePeriod, Alert } from "../../models/alert";
+
+interface AlertCardProps {
+  alert: Alert;
+}
+
+const AlertCard = (props: AlertCardProps): JSX.Element => {
+  const { alert } = props;
+
+  const formatEffect = (effect: string) => {
+    return effect
+      .toLowerCase()
+      .replace("_", " ")
+      .split(" ")
+      .map((str: string) => str[0].toUpperCase() + str.substring(1))
+      .join(" ");
+  };
+
+  const renderEffect = (effect: string, severity: string) => {
+    const formattedEffect = formatEffect(effect);
+    if (effect.toLowerCase() === "delay") {
+      return `${formattedEffect}—${severity}`;
+    }
+
+    return formattedEffect;
+  };
+
+  const renderActivePeriod = (activePeriod: ActivePeriod[]) => {
+    const startDate = moment(activePeriod[0].start);
+    let result;
+
+    const startDateHtml = (
+      <div className="alert-card__alert-details__active-period-start">
+        {startDate.format("l")} · {startDate.format("LT")}
+      </div>
+    );
+
+    if (activePeriod[0].end) {
+      const endDate = moment(activePeriod[0].end);
+      result = (
+        <>
+          {startDateHtml}
+          <div className="alert-card__alert-details__active-period-end">
+            <span className="alert-card__alert-details__active-period__to">
+              to{" "}
+            </span>
+            <span>
+              {endDate.format("l")} · {endDate.format("LT")}
+            </span>
+          </div>
+        </>
+      );
+    } else {
+      result = (
+        <>
+          {startDateHtml}
+          <div className="alert-card__alert-details__active-period-end">
+            Until further notice
+          </div>
+        </>
+      );
+    }
+
+    return result;
+  };
+
+  return (
+    <div className="alert-card">
+      <Container fluid className="alert-card__alert-details">
+        <Row>
+          <Col lg="auto">Icon</Col>
+          <Col lg={8}>
+            <div className="alert-card__alert-details__effect">
+              {renderEffect(alert.effect, alert.severity)}
+            </div>
+            <div className="alert-card__alert-details__header">
+              {alert.header}
+            </div>
+          </Col>
+          <Col lg="auto">
+            <div className="alert-card__alert-details__active-period">
+              {renderActivePeriod(alert.active_period)}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <div className="alert-card__place-details">
+        <div className="alert-card__place-details__alert-id">ID {alert.id}</div>
+        <div className="alert-card__place-details__place-count">
+          <span className="alert-card__place-details__place-count__number">
+            XX
+          </span>{" "}
+          <span className="alert-card__place-details__place-count__text">
+            places
+          </span>
+        </div>
+        <div className="alert-card__place-details__screen-count">
+          <span className="alert-card__place-details__screen-count__number">
+            XX
+          </span>{" "}
+          <span className="alert-card__place-details__screen-count__text">
+            screens
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AlertCard;
