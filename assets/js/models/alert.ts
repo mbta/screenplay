b67ca424a4c3aac89344f7246e8806a9aeaf0664
Paul Kim <pkim@mbta.com>
2022-09-16 11:17:28 -0400

    adjust alert model again and add informed entities

--- assets/js/models/alert.ts
+++ assets/js/models/alert.ts
@@ -1,8 +1,17 @@
 export interface Alert {
   id: string;
-  active_period: ActivePeriod[];
+  attributes: {
+    active_period: ActivePeriod[];
+    informed_entity: InformedEntity[];
+  };
 }
 interface ActivePeriod {
   start: string;
   end: string | null;
+}
+
+interface InformedEntity {
+  route: string;
+  route_type: number;
+  stop: string;
 }
