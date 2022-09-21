ca3e84e5daaebef322e2dbf9494aa73b737fd921
cmaddox5 <christian.maddox93@gmail.com>
2022-09-21 09:39:28 -0400

    Moved component logic to component brought in from merge.

--- assets/js/components/Dashboard/AlertsPage.tsx
+++ assets/js/components/Dashboard/AlertsPage.tsx
@@ -1,11 +1,22 @@
-import React, { ComponentType } from "react";
+import React, { ComponentType, useEffect, useState } from "react";
 import classNames from "classnames";
+import { Alert } from "../../models/alert";
+import AlertCard from "./AlertCard";
 
 interface Props {
   isVisible: boolean;
 }
 
 const AlertsPage: ComponentType<Props> = (props: Props) => {
+  const [alerts, setAlerts] = useState<Alert[]>([]);
+  useEffect(() => {
+    fetch("/api/alerts")
+      .then((response) => response.json())
+      .then((alertsList: []) => {
+        setAlerts(alertsList);
+      });
+  }, []);
+
   return (
     <div
       className={classNames("alerts-page", {
@@ -14,7 +25,9 @@ const AlertsPage: ComponentType<Props> = (props: Props) => {
     >
       <div className="page-content__header">Posted Alerts</div>
       <div className="page-content__body">
-        <div style={{ color: "white" }}>This will be posted alerts</div>
+        {alerts.map((alert) => (
+          <AlertCard key={alert.id} alert={alert} />
+        ))}
       </div>
     </div>
   );
