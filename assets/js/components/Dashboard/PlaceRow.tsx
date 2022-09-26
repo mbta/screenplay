4729668b36230ec4bc0236e1ebaa2a55c667af79
cmaddox5 <christian.maddox93@gmail.com>
2022-09-26 10:39:19 -0400

    Changed how map segments are rendered on PlaceRows.

--- assets/js/components/Dashboard/PlaceRow.tsx
+++ assets/js/components/Dashboard/PlaceRow.tsx
@@ -12,7 +12,8 @@ import classNames from "classnames";
 import { Place } from "../../models/place";
 import { Screen } from "../../models/screen";
 import ScreenDetail from "./ScreenDetail";
-import STATION_ORDER_BY_LINE, { Station } from "../../constants/stationOrder";
+import MapSegment from "./MapSegment";
+import STATION_ORDER_BY_LINE from "../../constants/stationOrder";
 
 interface PlaceRowProps {
   place: Place;
@@ -141,10 +142,14 @@ const PlaceRow = (props: PlaceRowProps): JSX.Element => {
     ));
   };
 
-  const getInlineMap = (place: Place, stationOrder: Station[]) => {
-    return stationOrder.find(
+  const getInlineMap = (place: Place, line: string) => {
+    const station = STATION_ORDER_BY_LINE[line].find(
       (station) => station.name.toLowerCase() === place.name.toLowerCase()
-    )?.inlineMap;
+    );
+
+    if (!station) return;
+
+    return <MapSegment station={station} line={line} />;
   };
 
   return (
@@ -182,14 +187,8 @@ const PlaceRow = (props: PlaceRowProps): JSX.Element => {
                   }
                 )}
               >
-                <img
-                  className="place-row__map-segment"
-                  src={`/images/inline-maps/${getInlineMap(
-                    props.place,
-                    STATION_ORDER_BY_LINE[props.filteredLine.toLowerCase()]
-                  )}.png`}
-                  alt=""
-                />
+                {props.filteredLine &&
+                  getInlineMap(props.place, props.filteredLine.toLowerCase())}
               </div>
             )}
             <div className="place-row__name" data-testid="place-name">
