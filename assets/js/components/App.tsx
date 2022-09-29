9b281102b7ef547092dd39c0a83dcc33f78a1a8a
cmaddox5 <christian.maddox93@gmail.com>
2022-09-29 11:43:53 -0400

    Added Sentry initialization to frontend.

--- assets/js/components/App.tsx
+++ assets/js/components/App.tsx
@@ -1,5 +1,6 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/react";
 
 const OutfrontTakeoverTool = React.lazy(
   () => import("./OutfrontTakeoverTool/OutfrontTakeoverTool")
@@ -30,6 +31,15 @@ class AppRoutes extends React.Component {
 }
 
 const App = (): ReactElement<HTMLDivElement> => {
+  const sentryDsn = document.getElementById("app")?.dataset.sentry;
+  useEffect(() => {
+    if (sentryDsn) {
+      Sentry.init({
+        dsn: sentryDsn,
+      });
+    }
+  }, [sentryDsn]);
+
   return (
     <BrowserRouter>
       <AppRoutes />
