1d2c930d516a78df14cd514bb0eed807dbea6bac
cmaddox5 <christian.maddox93@gmail.com>
2022-10-27 10:18:48 -0400

    Added ScreensByAlert data to alerts response.

--- lib/screenplay_web/controllers/alerts_api_controller.ex
+++ lib/screenplay_web/controllers/alerts_api_controller.ex
@@ -2,9 +2,17 @@ defmodule ScreenplayWeb.AlertsApiController do
   use ScreenplayWeb, :controller
 
   alias Screenplay.Alerts.Alert
+  alias Screenplay.Alerts.ScreensByAlert
 
   def index(conn, _params) do
     {:ok, alerts} = Alert.fetch()
-    json(conn, Enum.map(alerts, &Alert.to_full_map/1))
+
+    {:ok, screens_by_alerts} =
+      alerts |> Enum.map(& &1.id) |> ScreensByAlert.get_screens_by_alert()
+
+    json(conn, %{
+      alerts: Enum.map(alerts, &Alert.to_full_map/1),
+      screens_by_alert: screens_by_alerts
+    })
   end
 end
