fecf6ca96d28791e790ef8ebedddf8fbc38f3013
cmaddox5 <christian.maddox93@gmail.com>
2022-12-01 09:12:41 -0500

    Changed helper component to helper render function so only one place needs to setup routing/component wrapping.

---
+++ assets/tests/utils/renderWithScreenplayProvider.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ReactNode } from "react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { ScreenplayProvider } from "../../js/hooks/useScreenplayContext";
+import Dashboard from "../../js/components/Dashboard/Dashboard";
+import { render } from "@testing-library/react";
+
+export const renderWithScreenplayProvider = (children: ReactNode) => {
+  return render(
+    <MemoryRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ScreenplayProvider>
+              <Dashboard />
+            </ScreenplayProvider>
+          }
+        >
+          <Route index element={children} />
+        </Route>
+      </Routes>
+    </MemoryRouter>
+  );
+};
