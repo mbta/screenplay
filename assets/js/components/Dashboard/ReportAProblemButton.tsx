c651b1135637f590e566c61a78b9df816d5f7d56
cmaddox5 <christian.maddox93@gmail.com>
2022-12-08 10:42:14 -0500

    Changed Report A Problem to navigate to a different channel if user is an admin.

--- assets/js/components/Dashboard/ReportAProblemButton.tsx
+++ assets/js/components/Dashboard/ReportAProblemButton.tsx
@@ -3,10 +3,16 @@ import { Button } from "react-bootstrap";
 import { FlagFill } from "react-bootstrap-icons";
 
 const ReportAProblemButton = (): JSX.Element => {
+  const isAdmin = document.querySelector("meta[name=is-admin]");
+
+  const link = isAdmin
+    ? "https://mbta.slack.com/channels/screens-team-pios"
+    : "https://mbta.slack.com/channels/screens";
+
   return (
     <div className="report-a-problem-button">
       <Button
-        href="https://mbta.slack.com/channels/screens"
+        href={link}
         onClick={(e: SyntheticEvent) => e.stopPropagation()}
         target="_blank"
       >
