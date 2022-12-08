c2718c3c7e856abb2fbbaddd2756f8aa2ab7e395
cmaddox5 <christian.maddox93@gmail.com>
2022-12-08 10:21:27 -0500

    Removed redundant plug.

--- lib/screenplay_web/controllers/dashboard_controller.ex
+++ lib/screenplay_web/controllers/dashboard_controller.ex
@@ -1,33 +1,7 @@
 defmodule ScreenplayWeb.DashboardController do
   use ScreenplayWeb, :controller
 
-  alias Screenplay.Util
-
-  plug(:env_vars)
-
   def index(conn, _params) do
-    render(conn, "index.html", clarity_tag: System.get_env("CLARITY_TAG"))
-  end
-
-  defp env_vars(conn, _) do
-    username =
-      conn
-      |> get_session("username")
-      |> Util.trim_username()
-
-    dsn =
-      if Application.get_env(:screenplay, :record_sentry, false) do
-        Application.get_env(:screenplay, :sentry_frontend_dsn)
-      else
-        ""
-      end
-
-    conn
-    |> assign(:username, username)
-    |> assign(:environment_name, Application.get_env(:screenplay, :environment_name, "dev"))
-    |> assign(:sentry_frontend_dsn, dsn)
-    |> assign(:alerts_ui_url, Application.get_env(:screenplay, :alerts_ui_url))
-    |> assign(:screens_url, Application.get_env(:screenplay, :screens_url))
-    |> assign(:signs_ui_url, Application.get_env(:screenplay, :signs_ui_url))
+    render(conn, "index.html")
   end
 end
