38802d638f9431698dc27db4c018ad6805ef0050
Kim <pkim@mbta.com>
2023-11-08 16:19:05 -0500

    Add and delete logic

--- lib/screenplay/config/config.ex
+++ lib/screenplay/config/config.ex
@@ -1,12 +1,43 @@
 defmodule Screenplay.Config.PermanentConfig do
   @moduledoc false
+  alias Screenplay.Config.S3Fetch
 
-  def add_new_screen do
+  @spec add_new_screen(binary(), map(), binary()) :: :error | :ok
+  def add_new_screen(screen_id, screen, etag) do
+    case get_current_config(etag) do
+      {:ok, config} ->
+        config
+        |> Map.put(screen_id, Jason.decode!(screen))
+        |> S3Fetch.put_screens_config()
+
+      _ ->
+        :error
+    end
   end
 
   def edit_screen do
   end
 
-  def delete_screen do
+  @spec delete_screen(binary(), binary()) :: :error | :ok
+  def delete_screen(screen_id, etag) do
+    case get_current_config(etag) do
+      {:ok, config} ->
+        config
+        |> Map.delete(screen_id)
+        |> S3Fetch.put_screens_config()
+
+      _ ->
+        :error
+    end
+  end
+
+  defp get_current_config(etag) do
+    with {:ok, config, current_etag} <- S3Fetch.get_screens_config() do
+      if etag == current_etag,
+        do: {:ok, config},
+        else: :error
+    else
+      _ -> :error
+    end
   end
 end
--- lib/screenplay_web/controllers/config_controller.ex
+++ lib/screenplay_web/controllers/config_controller.ex
@@ -3,9 +3,14 @@ defmodule ScreenplayWeb.ConfigController do
 
   alias Screenplay.Config.PermanentConfig
 
-  def add(conn, _params) do
-    PermanentConfig.add_new_screen()
-    conn
+  def add(conn, %{"screen_id" => screen_id, "screen" => screen, "etag" => etag}) do
+    case PermanentConfig.add_new_screen(screen_id, screen, etag) do
+      :ok ->
+        send_resp(conn, 200, "OK")
+
+      :error ->
+        send_resp(conn, 400, "Unable to add new screen")
+    end
   end
 
   def edit(conn, _params) do
@@ -13,8 +18,13 @@ defmodule ScreenplayWeb.ConfigController do
     conn
   end
 
-  def delete(conn, _params) do
-    PermanentConfig.delete_screen()
-    conn
+  def delete(conn, %{"screen_id" => screen_id, "etag" => etag}) do
+    case PermanentConfig.delete_screen(screen_id, etag) do
+      :ok ->
+        send_resp(conn, 200, "OK")
+
+      :error ->
+        send_resp(conn, 400, "Unable to delete screen")
+    end
   end
 end
