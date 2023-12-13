0a77aecd3118a91f864ed96b660181ae0ea9f033
Christian Maddox <christian.maddox93@gmail.com>
2023-12-13 11:39:47 -0500

    Revert "[WIP] POC for using ETag to verify screens config version (#234)"
    This reverts commit 8d787e0a4987d6bc40d78b220cd48db97452c473.

--- lib/screenplay/config/config.ex
+++ lib/screenplay/config/config.ex
@@ -1,45 +1,12 @@
 defmodule Screenplay.Config.PermanentConfig do
   @moduledoc false
-  alias Screenplay.Config.S3Fetch
 
-  @spec add_new_screen(binary(), map(), binary()) ::
-          {:error, :etag_mismatch | :config_not_fetched | :config_not_written} | :ok
-  def add_new_screen(screen_id, screen, etag) do
-    case get_current_config(etag) do
-      {:ok, config} ->
-        config
-        |> Map.put(screen_id, Jason.decode!(screen))
-        |> S3Fetch.put_screens_config()
-
-      error ->
-        error
-    end
+  def add_new_screen do
   end
 
-  @spec delete_screen(binary(), binary()) ::
-          {:error, :etag_mismatch | :config_not_fetched | :config_not_written} | :ok
-  def delete_screen(screen_id, etag) do
-    case get_current_config(etag) do
-      {:ok, config} ->
-        config
-        |> Map.delete(screen_id)
-        |> S3Fetch.put_screens_config()
-
-      error ->
-        error
-    end
+  def edit_screen do
   end
 
-  defp get_current_config(etag) do
-    case S3Fetch.get_screens_config() do
-      {:ok, config, ^etag} ->
-        {:ok, config}
-
-      :error ->
-        {:error, :config_not_fetched}
-
-      _ ->
-        {:error, :etag_mismatch}
-    end
+  def delete_screen do
   end
 end
