b15b760dcca07e28fd76035bf39c9da40e735e76
cmaddox5 <christian.maddox93@gmail.com>
2023-12-19 08:49:25 -0500

    New endpoint for fetching existing screens for a place_id.

--- lib/screenplay_web/controllers/config_controller.ex
+++ lib/screenplay_web/controllers/config_controller.ex
@@ -1,7 +1,11 @@
 defmodule ScreenplayWeb.ConfigController do
   use ScreenplayWeb, :controller
 
+  alias ScreensConfig.Screen
+  alias ScreensConfig.V2.GlEink
   alias Screenplay.Config.PermanentConfig
+  alias Screenplay.ScreensConfig.Cache, as: ScreensConfigCache
+  alias Screenplay.PendingScreensConfig.Cache, as: PendingScreensConfigCache
 
   def index(conn, _params) do
     render(conn, "index.html")
@@ -38,4 +42,30 @@ defmodule ScreenplayWeb.ConfigController do
         send_resp(conn, 400, "S3 Operation Failed: Put")
     end
   end
+
+  def existing_screens(conn, %{"place_id" => place_id, "app_id" => app_id}) do
+    app_id_atom = String.to_existing_atom(app_id)
+
+    filter_fn = fn
+      {_, %Screen{app_id: ^app_id_atom} = config} ->
+        place_id_has_screen?(place_id, String.to_existing_atom(app_id), config)
+
+      _ ->
+        false
+    end
+
+    live_screens = ScreensConfigCache.screens(&filter_fn.(&1))
+    pending_screens = PendingScreensConfigCache.screens(&filter_fn.(&1))
+
+    json(conn, %{live_screens: live_screens, pending_screens: pending_screens})
+  end
+
+  defp place_id_has_screen?(place_id, :gl_eink_v2, %Screen{
+         app_params: %GlEink{footer: %{stop_id: stop_id}}
+       }),
+       do: place_id === stop_id
+
+  defp place_id_has_screen?(place_id, app_id, _),
+    do:
+      raise("place_id_has_screen/2 not implemented for app_id: #{app_id}, place_id: #{place_id}")
 end
