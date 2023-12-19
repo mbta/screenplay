bf8335246fd4718a518d0128bbee9f8bcdb2250c
cmaddox5 <christian.maddox93@gmail.com>
2023-12-19 11:08:44 -0500

    Made layout its own component.

--- assets/js/components/Dashboard/PermanentConfiguration/Workflows/GlEinkWorkflow.tsx
+++ assets/js/components/Dashboard/PermanentConfiguration/Workflows/GlEinkWorkflow.tsx
@@ -8,15 +8,62 @@ import { Place } from "../../../../models/place";
 import BottomActionBar from "../BottomActionBar";
 import { useNavigate } from "react-router-dom";
 
-const GlEinkWorkflow: ComponentType<WorkflowProps> = ({
+interface StationSelectPageProps {
+  places: Place[];
+  selectedPlaces: Set<string>;
+  setSelectedPlaces: React.Dispatch<React.SetStateAction<Set<string>>>;
+}
+
+const StationSelectPage: ComponentType<StationSelectPageProps> = ({
   places,
-}: WorkflowProps) => {
-  const [selectedPlaces, setSelectedPlaces] = useState<Set<string>>(new Set());
+  selectedPlaces,
+  setSelectedPlaces,
+}: StationSelectPageProps) => {
   const [sortDirection, setSortDirection] = useState<DirectionID>(0);
   const handleSearchResultClick = (place: SearchItem) => {
     const existingSelectedPlaces = new Set(selectedPlaces);
     setSelectedPlaces(existingSelectedPlaces.add(place.id));
   };
+  return (
+    <Container className="workflow-container">
+      <div className="mb-5">
+        <div className="h2 text-white mb-2">Select Green Line Stations</div>
+        <div className="body--regular text-white">
+          Green Line E-Ink screens can only be added at stations on Green Line
+          branches
+        </div>
+      </div>
+      <div className="search-bar-container mb-3">
+        <div className="body--medium mb-2">
+          Enter Station ID or name to select stations
+        </div>
+        <PlacesSearchBar
+          places={places}
+          selectedItems={Array.from(selectedPlaces)}
+          handleSearchResultClick={handleSearchResultClick}
+        />
+      </div>
+      <WorkflowPlacesList
+        places={places}
+        setSortDirection={setSortDirection}
+        sortDirection={sortDirection}
+        selectedPlaces={selectedPlaces}
+        onRowClick={(place: Place, checked: boolean) => {
+          // Make a new Set so React knows state was changed.
+          const newSet = new Set(selectedPlaces);
+          checked ? newSet.add(place.id) : newSet.delete(place.id);
+
+          setSelectedPlaces(newSet);
+        }}
+      />
+    </Container>
+  );
+};
+
+const GlEinkWorkflow: ComponentType<WorkflowProps> = ({
+  places,
+}: WorkflowProps) => {
+  const [selectedPlaces, setSelectedPlaces] = useState<Set<string>>(new Set());
 
   const navigate = useNavigate();
   const [configStep, setConfigStep] = useState<number>(0);
@@ -39,38 +86,11 @@ const GlEinkWorkflow: ComponentType<WorkflowProps> = ({
         setConfigStep(configStep + 1);
       };
       layout = (
-        <Container className="workflow-container">
-          <div className="mb-5">
-            <div className="h2 text-white mb-2">Select Green Line Stations</div>
-            <div className="body--regular text-white">
-              Green Line E-Ink screens can only be added at stations on Green
-              Line branches
-            </div>
-          </div>
-          <div className="search-bar-container mb-3">
-            <div className="body--medium mb-2">
-              Enter Station ID or name to select stations
-            </div>
-            <PlacesSearchBar
-              places={places}
-              selectedItems={Array.from(selectedPlaces)}
-              handleSearchResultClick={handleSearchResultClick}
-            />
-          </div>
-          <WorkflowPlacesList
-            places={places}
-            setSortDirection={setSortDirection}
-            sortDirection={sortDirection}
-            selectedPlaces={selectedPlaces}
-            onRowClick={(place: Place, checked: boolean) => {
-              // Make a new Set so React knows state was changed.
-              const newSet = new Set(selectedPlaces);
-              checked ? newSet.add(place.id) : newSet.delete(place.id);
-
-              setSelectedPlaces(newSet);
-            }}
-          />
-        </Container>
+        <StationSelectPage
+          places={places}
+          selectedPlaces={selectedPlaces}
+          setSelectedPlaces={setSelectedPlaces}
+        />
       );
       break;
     case 1:
