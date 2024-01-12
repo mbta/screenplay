9c12b3c23506c501b958ac65dfcc9833682e66ed
Paul Kim <pauljk704@gmail.com>
2024-01-12 10:16:50 -0500

    Bottom action bar (#242)
    * Added preview images.
    
    * Add custom ButtonImage component.
    
    * Added new page for perm config.
    
    * Changed value to existing variable.
    
    * Added a click handler to ButtonImage.
    
    * Gave page a conditional layout to be used later for navigation.
    
    * Removed files that shouldn't be there.
    
    * Moved route so Sidebar doesn't show.
    
    * Created AppBar CSS file.
    
    * Prevent wrapping as screen size decreases.
    
    * Added custom primary color.
    
    * Improved styling on ButtonImage.
    
    * Removed console.log.
    
    * Fixed import.
    
    * Fixed file name.
    
    * Centered content for all browsers.
    
    * Adjusted layout so it reduces to 2 columns at the md breakpoint.
    
    * Added a reuseable h3 class to match typography in designs.
    
    * Left the close button with defaults.
    
    * Added missing action.
    
    * Changed how Sidebar is hidden for configure-screens.
    
    * Added function for fetching places to be used by config pages.
    
    * Added a folder for Workflow components to live.
    
    * Fixed container height.
    
    * Made component for sort labels.
    
    * Additional classes for general typography.
    
    * Tweaked PlaceRow for better state management in select-box variant.
    
    * Moved function to Util.
    
    * New page for GL E-Ink perm config workflow.
    
    * Fixed container background when scrolling.
    
    * feat: Fetch station list for config pages (#240)
    
    * Changed how Sidebar is hidden for configure-screens.
    
    * Added function for fetching places to be used by config pages.
    
    * Throw when no case matches.
    
    * Fixed padding so text lines up with AppBar.
    
    * Gave count more font-weight.
    
    * Fixed some margins.
    
    * Changed interface name to something more general.
    
    * Disabled aggressive rule.
    
    * Added package.
    
    * Added a searchbar component.
    
    * Added searchbar to workflow.
    
    * Added callbacks so places are added to state when result is clicked.
    
    * Adjusted font weights.
    
    * Fixed search box not clearing after selecting item.
    
    * Added placeholder text.
    
    * Changed component name. Was a little too generic for what it actually does.
    
    * Changed min characters for results to show.
    
    * Add bottom action bar
    
    * create a separate css file
    
    * Rebase branch
    
    ---------
    
    Co-authored-by: cmaddox5 <christian.maddox93@gmail.com>

--- assets/js/components/Dashboard/PermanentConfiguration/Workflows/GlEinkWorkflow.tsx
+++ assets/js/components/Dashboard/PermanentConfiguration/Workflows/GlEinkWorkflow.tsx
@@ -2,8 +2,10 @@ import React, { ComponentType, useState } from "react";
 import { WorkflowProps } from "../ConfigureScreensPage";
 import { Container } from "react-bootstrap";
 import PlacesSearchBar, { SearchItem } from "../PlacesSearchBar";
-import WorkflowPlacesList from "../WorkflowPlacesList";
 import { DirectionID } from "../../../../models/direction_id";
+import BottomActionBar from "../BottomActionBar";
+import { useNavigate } from "react-router-dom";
+import WorkflowPlacesList from "../WorkflowPlacesList";
 import { Place } from "../../../../models/place";
 
 const GlEinkWorkflow: ComponentType<WorkflowProps> = ({
@@ -16,38 +18,91 @@ const GlEinkWorkflow: ComponentType<WorkflowProps> = ({
     setSelectedPlaces(existingSelectedPlaces.add(place.id));
   };
 
-  return (
-    <Container className="workflow-container">
-      <div className="mb-5">
-        <div className="h2 text-white mb-2">Select Green Line Stations</div>
-        <div className="body--regular text-white">
-          Green Line E-Ink screens can only be added at stations on Green Line
-          branches
-        </div>
-      </div>
-      <div className="search-bar-container mb-3">
-        <div className="body--medium mb-2">
-          Enter Station ID or name to select stations
-        </div>
-        <PlacesSearchBar
-          places={places}
-          handleSearchResultClick={handleSearchResultClick}
-        />
-      </div>
-      <WorkflowPlacesList
-        places={places}
-        setSortDirection={setSortDirection}
-        sortDirection={sortDirection}
-        selectedPlaces={selectedPlaces}
-        onRowClick={(place: Place, checked: boolean) => {
-          // Make a new Set so React knows state was changed.
-          const newSet = new Set(selectedPlaces);
-          checked ? newSet.add(place.id) : newSet.delete(place.id);
+  const navigate = useNavigate();
+  const [configStep, setConfigStep] = useState<number>(0);
 
-          setSelectedPlaces(newSet);
-        }}
-      />
-    </Container>
+  let backButtonLabel;
+  let forwardButtonLabel;
+  let cancelButtonLabel;
+  let onBack;
+  let onForward;
+  let onCancel;
+  let layout;
+  switch (configStep) {
+    case 0:
+      cancelButtonLabel = "Cancel";
+      forwardButtonLabel = "Next";
+      onCancel = () => {
+        navigate(-1);
+      };
+      onForward = () => {
+        setConfigStep(configStep + 1);
+      };
+      layout = (
+        <Container className="workflow-container">
+          <div className="mb-5">
+            <div className="h2 text-white mb-2">Select Green Line Stations</div>
+            <div className="body--regular text-white">
+              Green Line E-Ink screens can only be added at stations on Green
+              Line branches
+            </div>
+          </div>
+          <div className="search-bar-container mb-3">
+            <div className="body--medium mb-2">
+              Enter Station ID or name to select stations
+            </div>
+            <PlacesSearchBar
+              places={places}
+              handleSearchResultClick={handleSearchResultClick}
+            />
+          </div>
+          <WorkflowPlacesList
+            places={places}
+            setSortDirection={setSortDirection}
+            sortDirection={sortDirection}
+            selectedPlaces={selectedPlaces}
+            onRowClick={(place: Place, checked: boolean) => {
+              // Make a new Set so React knows state was changed.
+              const newSet = new Set(selectedPlaces);
+              checked ? newSet.add(place.id) : newSet.delete(place.id);
+
+              setSelectedPlaces(newSet);
+            }}
+          />
+        </Container>
+      );
+      break;
+    case 1:
+      backButtonLabel = "Back";
+      forwardButtonLabel = "Review Screens";
+      cancelButtonLabel = "Cancel";
+      onCancel = () => {
+        navigate(-1);
+      };
+      onBack = () => {
+        setConfigStep(configStep - 1);
+      };
+      onForward = () => {
+        navigate("/pending");
+      };
+      layout = <div>Configure Screens Page</div>;
+      break;
+  }
+
+  return (
+    <div>
+      {layout}
+      <div className="bottom-action-bar">
+        <BottomActionBar
+          backButtonLabel={backButtonLabel}
+          forwardButtonLabel={forwardButtonLabel}
+          cancelButtonLabel={cancelButtonLabel}
+          onCancel={onCancel}
+          onBack={onBack}
+          onForward={onForward}
+        ></BottomActionBar>
+      </div>
+    </div>
   );
 };
 
