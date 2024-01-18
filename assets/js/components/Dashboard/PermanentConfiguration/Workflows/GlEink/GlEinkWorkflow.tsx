90f013f355624b7077b9e77344e1b4c418d499fb
cmaddox5 <christian.maddox93@gmail.com>
2024-01-18 11:05:19 -0500

    Temporarily disable lint rule. Variables won't be used for this branch.

--- assets/js/components/Dashboard/PermanentConfiguration/Workflows/GlEink/GlEinkWorkflow.tsx
+++ assets/js/components/Dashboard/PermanentConfiguration/Workflows/GlEink/GlEinkWorkflow.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { ComponentType, useState } from "react";
 import { WorkflowProps } from "../../ConfigureScreensPage";
 import ConfigureScreensWorkflowPage, {
