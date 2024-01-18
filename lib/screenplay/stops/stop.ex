87ff02d55bb0d668429842464a945839fa8ea5d0
cmaddox5 <christian.maddox93@gmail.com>
2024-01-18 16:19:43 -0500

    Changed how platform IDs are fetched.

--- lib/screenplay/stops/stop.ex
+++ lib/screenplay/stops/stop.ex
@@ -4,21 +4,40 @@ defmodule Screenplay.Stops.Stop do
   alias Screenplay.V3Api
 
   def fetch_platform_ids_for_route_at_stop(stop_id, route_id) do
-    case V3Api.get_json("stops", %{
-           "filter[id]" => stop_id,
-           "include" => "child_stops",
-           "filter[route]" => route_id
+    case V3Api.get_json("route_patterns", %{
+           "include" => "representative_trip.stops",
+           "filter[route]" => route_id,
+           "filter[canonical]" => true
          }) do
-      {:ok, %{"data" => [data]}} ->
-        %{"relationships" => relationships} = data
+      {:ok, %{"included" => included_data}} ->
+        [{0, direction_0_platforms}, {1, direction_1_platforms}] =
+          included_data
+          |> Enum.filter(&(&1["type"] == "trip"))
+          |> Enum.map(fn trip ->
+            %{
+              "attributes" => %{"direction_id" => direction_id},
+              "relationships" => %{"stops" => %{"data" => platforms}}
+            } = trip
 
-        relationships
-        |> get_in(["child_stops", "data"])
-        |> Enum.map(fn platform ->
-          %{"id" => id} = platform
-          id
-        end)
-        |> Enum.filter(&match?({_, ""}, Integer.parse(&1)))
+            {direction_id, Enum.map(platforms, & &1["id"])}
+          end)
+          |> Enum.sort_by(&elem(&1, 0))
+
+        platform_ids_at_stop =
+          included_data
+          |> Enum.filter(
+            &match?(
+              %{
+                "type" => "stop",
+                "relationships" => %{"parent_station" => %{"data" => %{"id" => ^stop_id}}}
+              },
+              &1
+            )
+          )
+          |> Enum.map(& &1["id"])
+
+        {Enum.find(platform_ids_at_stop, &(&1 in direction_0_platforms)),
+         Enum.find(platform_ids_at_stop, &(&1 in direction_1_platforms))}
 
       _ ->
         []
