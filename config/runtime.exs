03f11c60b1862876f57dc33a0645de8db1c5af6a
cmaddox5 <christian.maddox93@gmail.com>
2024-02-01 13:21:53 -0500

    Removed hardcoded redirect_uri.

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,8 +17,7 @@ env = System.get_env("ENVIRONMENT_NAME")
 keycloak_opts = [
   issuer: :keycloak_issuer,
   client_id: System.fetch_env!("KEYCLOAK_CLIENT_ID"),
-  client_secret: System.fetch_env!("KEYCLOAK_CLIENT_SECRET"),
-  redirect_uri: "https://screenplay-dev-green.mbtace.com/auth/keycloak/callback"
+  client_secret: System.fetch_env!("KEYCLOAK_CLIENT_SECRET")
 ]
 
 config :ueberauth_oidcc,
