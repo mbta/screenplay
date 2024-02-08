c046ff5a8b4540f376bd2125204e2b16b09d6ca4
cmaddox5 <christian.maddox93@gmail.com>
2024-02-08 10:44:19 -0500

    Added a button to help with testing server logic.

--- assets/js/components/Dashboard/PendingScreensPage.tsx
+++ assets/js/components/Dashboard/PendingScreensPage.tsx
@@ -1,9 +1,19 @@
 import React, { ComponentType } from "react";
+import { publishScreensForPlace } from "../../utils/api";
 
 const PendingScreensPage: ComponentType = () => {
   return (
     <div>
       <div className="page-content__header">Pending</div>
+      <button
+        onClick={() =>
+          publishScreensForPlace("place-hsmnl").then((statusText) =>
+            alert(statusText)
+          )
+        }
+      >
+        Publish
+      </button>
     </div>
   );
 };
