5c733fdd41e782f1d89095540dad048dbc21325c
cmaddox5 <christian.maddox93@gmail.com>
2024-02-12 10:29:43 -0500

    Revert "Added a button to help with testing server logic."
    This reverts commit c046ff5a8b4540f376bd2125204e2b16b09d6ca4.

--- assets/js/components/Dashboard/PendingScreensPage.tsx
+++ assets/js/components/Dashboard/PendingScreensPage.tsx
@@ -1,19 +1,9 @@
 import React, { ComponentType } from "react";
-import { publishScreensForPlace } from "../../utils/api";
 
 const PendingScreensPage: ComponentType = () => {
   return (
     <div>
       <div className="page-content__header">Pending</div>
-      <button
-        onClick={() =>
-          publishScreensForPlace("place-hsmnl").then((statusText) =>
-            alert(statusText)
-          )
-        }
-      >
-        Publish
-      </button>
     </div>
   );
 };
