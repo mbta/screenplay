29393e35220c289c20b7b63a1d8115c39c1e0dfd
cmaddox5 <christian.maddox93@gmail.com>
2024-02-16 14:51:38 -0500

    Tackled most of the PendingScreenDetail component.

--- assets/js/components/Dashboard/PendingScreenDetail.tsx
+++ assets/js/components/Dashboard/PendingScreenDetail.tsx
@@ -1,13 +1,18 @@
 // Note: component name is a bit unclear--this can show detail for both pending and live screens,
 // but its presentation is particular to the pending screens page.
 
-import React from "react";
-import { Screen } from "../../models/screen";
+import React, { ComponentType, SyntheticEvent } from "react";
 import { ScreenConfiguration } from "../../models/screen_configuration";
 import ScreenSimulation from "./ScreenSimulation";
-import { fetchExistingScreens } from "../../utils/api";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import { capitalize } from "../../util";
+import OpenInTabButton from "./OpenInTabButton";
+import CopyLinkButton from "./CopyLinkButton";
+import { useScreenplayDispatchContext } from "../../hooks/useScreenplayContext";
 
 interface Props {
+  screenID: string;
+  isLive: boolean;
   config: ScreenConfiguration;
   // I'm still not super clear what the purpose of this is, or how/where to store it...
   // We now have two different ways of hiding screens from the places list:
@@ -21,12 +26,103 @@ interface Props {
   onClickHideOnPlacesPage: () => void;
 }
 
-const PendingScreenDetail = (props: Props): JSX.Element => {
-  return (
-    <div className="pending-screen-detail">
+const PendingScreenDetail: ComponentType<Props> = ({
+  screenID,
+  isLive,
+  config,
+  isHiddenOnPlacesPage,
+  onClickHideOnPlacesPage,
+}: Props): JSX.Element => {
+  const dispatch = useScreenplayDispatchContext();
+  const screensUrl = document
+    .querySelector("meta[name=screens-url]")
+    ?.getAttribute("content");
 
+  const formatDirectionID = () => {
+    switch (config.app_params.header.direction_id) {
+      case 0:
+        return "Westbound";
+      case 1:
+        return "Eastbound";
+      default:
+        return "";
+    }
+  };
+
+  const queueToastExpiration = () => {
+    setTimeout(
+      () =>
+        dispatch({
+          type: "SHOW_LINK_COPIED",
+          showLinkCopied: false,
+        }),
+      5000
+    );
+  };
+
+  const simulationUrl = isLive
+    ? `${screensUrl}/v2/screen/${screenID}/simulation`
+    : `${screensUrl}/v2/screen/pending/${screenID}/simulation`;
+
+  return (
+    <div
+      className="pending-screen-detail"
+      onClick={(e: SyntheticEvent) => e.stopPropagation()}
+    >
+      <Container className="h5">
+        <Row>
+          <Col className="d-flex justify-content-start">
+            {formatDirectionID()}{" "}
+            {capitalize(config.app_params.platform_location ?? "")}
+          </Col>
+          <Col className="d-flex justify-content-end">
+            {isLive ? "Live · Read-only" : "Pending"}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            Screen ID: <span className="body--regular">{screenID}</span>
+          </Col>
+        </Row>
+        <Row className="screen-url">
+          <Col lg="auto" className="pe-3">
+            Screen URL:
+          </Col>
+          <Col lg="auto" className="p-0 pe-3">
+            <a className="url body--regular" href={simulationUrl}>
+              https://.../{screenID}
+            </a>
+          </Col>
+          <Col lg="1" className="p-0">
+            <OpenInTabButton url={simulationUrl} />
+          </Col>
+          <Col lg="auto" className="p-0">
+            <CopyLinkButton
+              url={simulationUrl}
+              queueToastExpiration={queueToastExpiration}
+            />
+          </Col>
+        </Row>
+        {!isLive && (
+          <Row>
+            <Col lg="auto">
+              <Form.Check
+                checked={isHiddenOnPlacesPage}
+                readOnly
+                onClick={onClickHideOnPlacesPage}
+              />
+            </Col>
+            <Col>Hide on Live Screens</Col>
+          </Row>
+        )}
+        <Row className="screen-simulation">
+          <ScreenSimulation
+            screen={{ id: screenID, type: config.app_id, disabled: false }}
+          />
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
 export default PendingScreenDetail;
--- assets/js/components/Dashboard/PendingScreensPlaceRowAccordion.tsx
+++ assets/js/components/Dashboard/PendingScreensPlaceRowAccordion.tsx
@@ -1,8 +1,9 @@
-import React, { ComponentType } from "react";
+import React, { ComponentType, useContext, useState } from "react";
 import { ScreenConfiguration } from "../../models/screen_configuration";
 import PendingScreenDetail from "./PendingScreenDetail";
 import {
   Accordion,
+  AccordionContext,
   Button,
   Col,
   Container,
@@ -13,6 +14,7 @@ import { Place } from "../../models/place";
 import { AccordionToggle } from "./PlaceRow";
 import { capitalizeTerminalStops } from "../../util";
 import { PencilSquare } from "react-bootstrap-icons";
+import classNames from "classnames";
 
 interface Props {
   place: Place;
@@ -31,6 +33,11 @@ const PendingScreensPlaceRowAccordion: ComponentType<Props> = ({
   appID,
   screens,
 }: Props) => {
+  const [hiddenFromScreenplayIDs, setHiddenFromScreenplayIDs] = useState<
+    string[]
+  >([]);
+  const { activeEventKey } = useContext(AccordionContext);
+  const isOpen = activeEventKey?.includes(place.id);
   const onRowClick = useAccordionButton(place.id);
   const formatAppID = () => {
     switch (appID) {
@@ -42,10 +49,26 @@ const PendingScreensPlaceRowAccordion: ComponentType<Props> = ({
     }
   };
 
+  const hideCheckboxOnClick = (screenID: string) => {
+    if (hiddenFromScreenplayIDs.includes(screenID)) {
+      setHiddenFromScreenplayIDs((prevState) =>
+        prevState.filter((id) => id !== screenID)
+      );
+    } else {
+      setHiddenFromScreenplayIDs((prevState) => [...prevState, screenID]);
+    }
+  };
+
   // TODO: Designs show a "Type" field in accordion header, e.g. "Type: GL E-Ink".
   // This doesn't make sense for Places with multiple screen types, check w/ Mary what to do about it.
   return (
-    <div className="pending-screens-place-row-accordion" onClick={onRowClick}>
+    <div
+      className={classNames(
+        "pending-screens-place-row-accordion",
+        isOpen ? "open" : ""
+      )}
+      onClick={onRowClick}
+    >
       <div className="pending-screens-place-row-accordion__header">
         <Container fluid>
           <Row className="align-items-center text-white">
@@ -73,14 +96,19 @@ const PendingScreensPlaceRowAccordion: ComponentType<Props> = ({
       </div>
       <Accordion.Collapse eventKey={place.id}>
         <>
-          {screens.map((screen: LiveOrPendingScreen) => (
-            <PendingScreenDetail
-              {...screen}
-              isHiddenOnPlacesPage={false}
-              onClickHideOnPlacesPage={() => undefined}
-              key={screen.screenID}
-            />
-          ))}
+          {isOpen &&
+            screens.map((screen: LiveOrPendingScreen) => (
+              <PendingScreenDetail
+                {...screen}
+                isHiddenOnPlacesPage={hiddenFromScreenplayIDs.includes(
+                  screen.screenID
+                )}
+                onClickHideOnPlacesPage={() =>
+                  hideCheckboxOnClick(screen.screenID)
+                }
+                key={screen.screenID}
+              />
+            ))}
         </>
       </Accordion.Collapse>
     </div>
