f47e523dfa853a9649d0a4c5eb1009c2d9c23d3c
cmaddox5 <christian.maddox93@gmail.com>
2024-02-28 13:24:36 -0500

    Revert "Revert later."
    This reverts commit c18429774f97ae0acb3f40fe891dd0cf04cc7882.

--- assets/js/components/Dashboard/PendingScreensPage.tsx
+++ assets/js/components/Dashboard/PendingScreensPage.tsx
@@ -1,23 +1,9 @@
 import React, { ComponentType } from "react";
-import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 
 const PendingScreensPage: ComponentType = () => {
-  const navigate = useNavigate();
-  const appId = "gl-eink";
-
   return (
     <div>
       <div className="page-content__header">Pending</div>
-      <Button
-        onClick={() => {
-          navigate(`/configure-screens/${appId}`, {
-            state: { place_id: "place-hsmnl" },
-          });
-        }}
-      >
-        Navigate
-      </Button>
     </div>
   );
 };
