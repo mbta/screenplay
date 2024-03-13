455a8178daa4bfba751fa27124626b32c8f60bc3
Jon Zimbel <jzimbel@mbta.com>
2024-03-13 08:41:14 -0400

    Remove peanut gallery commentary

--- assets/js/components/Dashboard/PendingScreenDetail.tsx
+++ assets/js/components/Dashboard/PendingScreenDetail.tsx
@@ -15,14 +15,6 @@ interface Props {
   screenID: string;
   isLive: boolean;
   config: ScreenConfiguration;
-  // I'm still not super clear what the purpose of this is, or how/where to store it...
-  // We now have two different ways of hiding screens from the places list:
-  // - When hidden via the old admin tool, the screen is excluded from places_and_screens.json
-  // - When hidden this new way, the screen is known to Screenplay--and thus needs a full, valid
-  //   config + Screenplay-relevant metadata, since we show info about it here on
-  //   the pending page--but ignored when rendering the places list.
-  //
-  // This seems messy and I'm not sure how to explain the issue to design.
   isHiddenOnPlacesPage: boolean;
   onClickHideOnPlacesPage: () => void;
 }
