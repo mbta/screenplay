acc75b05ecd4024025a785397dd8325d93e54159
cmaddox5 <christian.maddox93@gmail.com>
2024-03-21 10:38:14 -0400

    Added missing test config.

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,8 @@ config :screenplay,
   alerts_fetch_module: Screenplay.OutfrontTakeoverTool.Alerts.LocalFetch,
   local_alerts_path_spec: {:test, "alerts.json"},
   config_fetcher: Screenplay.Config.LocalFetch,
-  api_v3_url: [:no_api_requests_allowed_during_testing]
+  api_v3_url: [:no_api_requests_allowed_during_testing],
+  sftp_client_module: Screenplay.Outfront.FakeSFTPClient
 
 config :ueberauth, Ueberauth,
   providers: [
