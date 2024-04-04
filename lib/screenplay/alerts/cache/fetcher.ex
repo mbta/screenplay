4ab60c3d45a85a2c7d9a1efc5b3d3725d8bbd4e5
cmaddox5 <christian.maddox93@gmail.com>
2024-04-04 16:21:18 -0400

    Made fetcher more testable.

--- lib/screenplay/alerts/cache/fetcher.ex
+++ lib/screenplay/alerts/cache/fetcher.ex
@@ -3,35 +3,45 @@ defmodule Screenplay.Alerts.Cache.Fetcher do
 
   use GenServer
 
+  alias Screenplay.V3Api
   alias Screenplay.Alerts.Alert
   alias Screenplay.Alerts.Cache.Store
 
-  @update_interval_ms 4_000
+  @default_opts [
+    get_json_fn: &V3Api.get_json/2,
+    update_interval_ms: 4_000,
+    update_fn: &Store.update/1
+  ]
 
   # Client
   def start_link(opts \\ []) do
+    opts = Keyword.merge(@default_opts, opts)
     GenServer.start_link(__MODULE__, opts)
   end
 
   # Server
   @impl true
-  def init(state) do
-    schedule_fetch(@update_interval_ms)
+  def init(opts) do
+    get_json_fn = Keyword.get(opts, :get_json_fn)
+    update_interval_ms = Keyword.get(opts, :update_interval_ms)
+    update_fn = Keyword.get(opts, :update_fn)
 
-    {:ok, state}
+    schedule_fetch(update_interval_ms)
+
+    {:ok, {get_json_fn, update_interval_ms, update_fn}}
   end
 
   @impl true
-  def handle_info(:fetch, state) do
-    case Alert.fetch() do
+  def handle_info(:fetch, {get_json_fn, update_interval_ms, update_fn} = state) do
+    case Alert.fetch(get_json_fn) do
       {:ok, alerts} ->
-        Store.update(alerts)
+        update_fn.(alerts)
 
       :error ->
         _ = Logger.info("#{__MODULE__} error fetching alerts")
     end
 
-    schedule_fetch(@update_interval_ms)
+    schedule_fetch(update_interval_ms)
 
     {:noreply, state, :hibernate}
   end
