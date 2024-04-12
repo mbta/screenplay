50973c59f454f2ddb50dd78adc3e89d72f6bff35
cmaddox5 <christian.maddox93@gmail.com>
2024-04-12 14:00:22 -0400

    Temp Sentry test.

--- lib/screenplay_web/controllers/alerts_controller.ex
+++ lib/screenplay_web/controllers/alerts_controller.ex
@@ -1,7 +1,10 @@
 defmodule ScreenplayWeb.AlertsController do
   use ScreenplayWeb, :controller
 
+  require Logger
+
   def index(conn, _params) do
+    Logger.error("Testing for Sentry")
     render(conn, "index.html")
   end
 end
