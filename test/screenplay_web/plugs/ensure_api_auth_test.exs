e36df7f5f7833df657a55f1eb2dcc6e70f90d27e
cmaddox5 <christian.maddox93@gmail.com>
2024-05-06 15:16:28 -0400

    Credo.

--- test/screenplay_web/plugs/ensure_api_auth_test.exs
+++ test/screenplay_web/plugs/ensure_api_auth_test.exs
@@ -1,15 +1,17 @@
 defmodule ScreenplayWeb.Plugs.EnsureApiAuthTest do
   use ScreenplayWeb.ConnCase
 
+  alias ScreenplayWeb.Plugs.EnsureApiAuth
+
   describe "init/1" do
     test "passes options through unchanged" do
-      assert ScreenplayWeb.Plugs.EnsureApiAuth.init([]) == []
+      assert EnsureApiAuth.init([]) == []
     end
   end
 
   describe "call/2" do
     test "returns 403 when x-api-key header is missing", %{conn: conn} do
-      conn = ScreenplayWeb.Plugs.EnsureApiAuth.call(conn, [])
+      conn = EnsureApiAuth.call(conn, [])
 
       assert %{status: 403, halted: true, resp_body: "Invalid API key"} = conn
     end
@@ -18,14 +20,14 @@ defmodule ScreenplayWeb.Plugs.EnsureApiAuthTest do
       conn =
         conn
         |> Plug.Conn.put_req_header("x-api-key", "1234")
-        |> ScreenplayWeb.Plugs.EnsureApiAuth.call([])
+        |> EnsureApiAuth.call([])
 
       assert %{status: 403, halted: true, resp_body: "Invalid API key"} = conn
     end
 
     @tag :api_authenticated
     test "returns conn unchanged if x-api-key header matches app API key", %{conn: conn} do
-      assert ScreenplayWeb.Plugs.EnsureApiAuth.call(conn, []) == conn
+      assert EnsureApiAuth.call(conn, []) == conn
     end
   end
 end
