bc1216dd9586d08c633e9a5f4fc25435465021be
cmaddox5 <christian.maddox93@gmail.com>
2024-05-13 10:33:49 -0400

    Added helper for tests that use DB.

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -34,13 +34,6 @@ defmodule ScreenplayWeb.ConnCase do
   end
 
   setup tags do
-    alias Ecto.Adapters.SQL.Sandbox
-    :ok = Sandbox.checkout(Screenplay.Repo)
-
-    unless tags[:async] do
-      Sandbox.mode(Draft.Repo, {:shared, self()})
-    end
-
     {conn, user} =
       cond do
         tags[:authenticated_emergency_admin] ->
