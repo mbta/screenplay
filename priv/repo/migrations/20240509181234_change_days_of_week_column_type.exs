17f77d1c5193a99b878693d63972e5b354588f51
cmaddox5 <christian.maddox93@gmail.com>
2024-05-17 10:11:20 -0400

    Simplified execute call.

--- priv/repo/migrations/20240509181234_change_days_of_week_column_type.exs
+++ priv/repo/migrations/20240509181234_change_days_of_week_column_type.exs
@@ -2,14 +2,6 @@ defmodule Screenplay.Repo.Migrations.ChangeDaysOfWeekColumnType do
   use Ecto.Migration
 
   def change do
-    execute(&execute_up/0, &execute_down/0)
+    execute "ALTER TABLE pa_message ALTER COLUMN days_of_week TYPE integer[] USING (days_of_week::integer[])"
   end
-
-  defp execute_up do
-    repo().query!(
-      "ALTER TABLE pa_message ALTER COLUMN days_of_week TYPE integer[] USING (days_of_week::integer[])"
-    )
-  end
-
-  defp execute_down, do: nil
 end
