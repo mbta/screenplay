f95b7202f042f3fc2932ff44d83f7f8185bd5dc9
cmaddox5 <christian.maddox93@gmail.com>
2024-05-29 13:27:17 -0400

    Revert "tweak: Auth rework (#348)"
    This reverts commit a13ce355e5da93f7e6c62b69a24df10276e885c8.

--- lib/screenplay_web/auth_manager/error_handler.ex
+++ lib/screenplay_web/auth_manager/error_handler.ex
@@ -5,35 +5,10 @@ defmodule ScreenplayWeb.AuthManager.ErrorHandler do
 
   use ScreenplayWeb, :plug
 
-  alias ScreenplayWeb.Router.Helpers, as: Routes
-
   @behaviour Guardian.Plug.ErrorHandler
 
   @impl Guardian.Plug.ErrorHandler
-  def auth_error(conn, error, _opts) do
-    auth_params = auth_params_for_error(error)
-
-    Phoenix.Controller.redirect(conn,
-      to: Routes.auth_path(conn, :request, "keycloak", auth_params)
-    )
-  end
-
-  def auth_params_for_error({:invalid_token, {:auth_expired, sub}}) do
-    # if we know the user who was logged in before, provide that upstream to simplify
-    # logging in
-    %{
-      prompt: "login",
-      login_hint: sub
-    }
-  end
-
-  def auth_params_for_error({:unauthenticated, _}) do
-    %{}
-  end
-
-  def auth_params_for_error(_) do
-    %{
-      prompt: "login"
-    }
+  def auth_error(conn, {_type, _reason}, _opts) do
+    Phoenix.Controller.redirect(conn, to: ~p"/auth/keycloak")
   end
 end
