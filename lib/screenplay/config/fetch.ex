b187ca62f770d97e2babd75ccefe6bb488700de0
cmaddox5 <christian.maddox93@gmail.com>
2024-07-02 11:32:26 -0400

    Fixed type.

--- lib/screenplay/config/fetch.ex
+++ lib/screenplay/config/fetch.ex
@@ -5,7 +5,8 @@ defmodule Screenplay.Config.Fetch do
 
   @type version_id :: String.t()
 
-  @callback get_places_and_screens() :: {:ok, list(map()), String.t()} | :error
+  # All logic assumes that the configs will always be a list of maps.
+  @callback get_places_and_screens() :: {:ok, list(map()), version_id()} | :error
   @callback get_locations() :: {:ok, list(map()), version_id()} | :error
   @callback get_place_descriptions() :: {:ok, list(map()), version_id()} | :error
   @callback put_config(list(PlaceAndScreens.t())) :: :ok | :error
