ca869629dbdf6a738e6a4387451777215d213a10
cmaddox5 <christian.maddox93@gmail.com>
2024-07-08 09:41:40 -0400

    Tweaked Parser so it doesn't fail when there are no alerts in the response.

--- test/screenplay/alerts/alert_test.exs
+++ test/screenplay/alerts/alert_test.exs
@@ -63,6 +63,12 @@ defmodule Screenplay.Alerts.AlertTest do
               ]} = Alert.fetch(get_json_fn)
     end
 
+    test "returns {:ok, []} if fetch function returns an empty alerts list" do
+      get_json_fn = fn _, _ -> {:ok, %{"data" => []}} end
+
+      assert {:ok, []} = Alert.fetch(get_json_fn)
+    end
+
     test "returns :error if fetch function returns :error", context do
       %{
         x_get_json_fn: x_get_json_fn
