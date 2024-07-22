1569f3531aba96da2e461fc567eb00d700da5142
cmaddox5 <christian.maddox93@gmail.com>
2024-07-22 10:23:36 -0400

    Added header to new page.

--- assets/js/components/Dashboard/NewPaMessage/SelectZonesPage.tsx
+++ assets/js/components/Dashboard/NewPaMessage/SelectZonesPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Page } from "./types";
+import { Button } from "react-bootstrap";
 
 interface Props {
   zones: string[];
@@ -7,8 +8,41 @@ interface Props {
   navigateTo: (page: Page) => void;
 }
 
-const SelectZonesPage = ({ navigateTo }: Props) => {
-  return <div>Select Zones Page</div>;
+const SelectZonesPage = ({ zones, setZones, navigateTo }: Props) => {
+  return (
+    <div className="select-zones-page">
+      <div className="header">
+        <div className="title-and-edit">
+          <div className="title">Review Zones</div>
+          <div>
+            <Button
+              className="edit-button"
+              onClick={() => navigateTo(Page.STATIONS)}
+            >
+              Edit Stations
+            </Button>
+          </div>
+        </div>
+        <div className="buttons">
+          <Button
+            className="cancel-button"
+            onClick={() => {
+              setZones([]);
+              navigateTo(Page.NEW);
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            className="submit-button"
+            onClick={() => navigateTo(Page.NEW)}
+          >
+            Done
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SelectZonesPage;
