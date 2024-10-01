775820e9cebb5793cb3612a46139311002afec79
cmaddox5 <christian.maddox93@gmail.com>
2024-10-01 13:07:57 -0400

    Define mock modules in separate file.

---
+++ test/support/mocks.ex
@@ -0,0 +1,3 @@
+Mox.defmock(Screenplay.RoutePatterns.Mock, for: Screenplay.RoutePatterns.Behaviour)
+Mox.defmock(Screenplay.Stops.Mock, for: Screenplay.Stops.Behaviour)
+Mox.defmock(Screenplay.Routes.Mock, for: Screenplay.Routes.Behaviour)
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,3 @@
-Mox.defmock(Screenplay.RoutePatterns.Mock, for: Screenplay.RoutePatterns.Behaviour)
-Mox.defmock(Screenplay.Stops.Mock, for: Screenplay.Stops.Behaviour)
-Mox.defmock(Screenplay.Routes.Mock, for: Screenplay.Routes.Behaviour)
 Application.put_env(:screenplay, :route_pattern_mod, Screenplay.RoutePatterns.Mock)
 Application.put_env(:screenplay, :stops_mod, Screenplay.Stops.Mock)
 Application.put_env(:screenplay, :routes_mod, Screenplay.Routes.Mock)
