cf49a4e78fa169c9797e9d3a3643ad8727912581
cmaddox5 <christian.maddox93@gmail.com>
2024-10-10 13:10:23 -0400

    Check time since typed in password and error if over configured times.

--- lib/screenplay_web/controllers/auth_controller.ex
+++ lib/screenplay_web/controllers/auth_controller.ex
@@ -6,7 +6,13 @@ defmodule ScreenplayWeb.AuthController do
   def callback(conn = %{assigns: %{ueberauth_auth: auth}}, _params) do
     username = auth.uid
     name = auth.info.name
-    session_ttl_hours = 24 * 30
+
+    auth_time =
+      Map.get(
+        auth.extra.raw_info.claims,
+        "auth_time",
+        auth.extra.raw_info.claims["iat"]
+      )
 
     keycloak_client_id =
       get_in(Application.get_env(:ueberauth_oidcc, :providers), [:keycloak, :client_id])
@@ -18,11 +24,12 @@ defmodule ScreenplayWeb.AuthController do
     Plug.Conn.delete_session(conn, :previous_path)
 
     conn
+    |> configure_session(drop: true)
     |> Guardian.Plug.sign_in(
       ScreenplayWeb.AuthManager,
       username,
-      %{roles: roles},
-      ttl: {session_ttl_hours, :hours}
+      %{auth_time: auth_time, roles: roles},
+      ttl: {1, :minute}
     )
     |> Plug.Conn.put_session(:username, name || username)
     # Redirect to whatever page they came from
