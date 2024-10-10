f489c4780dd6ca5cc775c92613a9164c9b06f54f
cmaddox5 <christian.maddox93@gmail.com>
2024-10-10 13:10:31 -0400

    Redirect to login page on error.

--- lib/screenplay_web/auth_manager/error_handler.ex
+++ lib/screenplay_web/auth_manager/error_handler.ex
@@ -8,7 +8,22 @@ defmodule ScreenplayWeb.AuthManager.ErrorHandler do
   @behaviour Guardian.Plug.ErrorHandler
 
   @impl Guardian.Plug.ErrorHandler
-  def auth_error(conn, {_type, _reason}, _opts) do
-    Phoenix.Controller.redirect(conn, to: ~p"/auth/keycloak")
+  def auth_error(conn, error, _opts) do
+    auth_params = auth_params_for_error(error)
+    Phoenix.Controller.redirect(conn, to: ~p"/auth/keycloak?#{auth_params}")
+  end
+
+  def auth_params_for_error({:invalid_token, {:auth_expired, sub}}) do
+    # if we know the user who was logged in before, provide that upstream to simplify
+    # logging in
+    "prompt=login&login_hint=#{sub}"
+  end
+
+  def auth_params_for_error({:unauthenticated, _}) do
+    ""
+  end
+
+  def auth_params_for_error(_) do
+    "prompt=login"
   end
 end
