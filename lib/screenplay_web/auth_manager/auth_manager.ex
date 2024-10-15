24a7afbec9f9efa3653096325568f0d72bc94aa7
Christian Maddox <christian.maddox93@gmail.com>
2024-10-15 13:26:57 -0400

    chore: Refresh session on request (#520)
    * Update ueberauth deps.
    
    * Ueberauth config updates.
    
    * Check time since typed in password and error if over configured times.
    
    * Redirect to login page on error.
    
    * Add plug to refresh idle token on request.
    
    * Add iat to claims in fake strategy.
    
    * Fix test.
    
    * Credo.
    
    * Forgot to delete old config.
    
    * Change value to string.
    
    * Use compile_env.
    
    * Revert "Use compile_env."
    
    This reverts commit 879513ce31a451a3e10eace5e8fa58209647d45a.
    
    * Safely access config at compile time.

--- lib/screenplay_web/auth_manager/auth_manager.ex
+++ lib/screenplay_web/auth_manager/auth_manager.ex
@@ -12,6 +12,10 @@ defmodule ScreenplayWeb.AuthManager do
     "pa-message-admin" => :pa_message_admin
   }
 
+  @max_session_time Application.compile_env!(:screenplay, [__MODULE__, :max_session_time])
+  @idle_time Application.compile_env!(:screenplay, [__MODULE__, :idle_time])
+
+  @impl true
   @spec subject_for_token(
           resource :: Guardian.Token.resource(),
           claims :: Guardian.Token.claims()
@@ -20,6 +24,7 @@ defmodule ScreenplayWeb.AuthManager do
     {:ok, resource}
   end
 
+  @impl true
   @spec resource_from_claims(claims :: Guardian.Token.claims()) ::
           {:error, :invalid_claims} | {:ok, String.t()}
   def resource_from_claims(%{"sub" => username}) do
@@ -35,5 +40,20 @@ defmodule ScreenplayWeb.AuthManager do
 
   def claims_access_level(_claims) do
     []
+  end
+
+  @impl true
+  def verify_claims(claims = %{"iat" => iat, "auth_time" => auth_time}, _opts) do
+    now = System.system_time(:second)
+    # auth_time is when the user entered their password at the SSO provider
+    auth_time_expires = auth_time + @max_session_time
+    # iat is when the token was issued
+    iat_expires = iat + @idle_time
+    # did either timeout expire?
+    if min(auth_time_expires, iat_expires) < now do
+      {:error, {:auth_expired, claims["sub"]}}
+    else
+      {:ok, claims}
+    end
   end
 end
