0785d9520b70645868db8d5d0258f0bd050d8b65
cmaddox5 <christian.maddox93@gmail.com>
2024-10-24 08:50:30 -0400

    Converted eslintrc to new format.

---
+++ assets/eslint.config.mjs
@@ -0,0 +1,85 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import react from "eslint-plugin-react";
+import jsxA11Y from "eslint-plugin-jsx-a11y";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: [
+        "**/webpack.config.js",
+        "**/.eslintrc.js",
+        "**/socket.js",
+        "**/node_modules",
+        "**/coverage",
+    ],
+}, ...fixupConfigRules(compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+)), {
+    plugins: {
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        react: fixupPluginRules(react),
+        "jsx-a11y": fixupPluginRules(jsxA11Y),
+    },
+
+    linterOptions: {
+        reportUnusedDisableDirectives: true,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        "no-console": 0,
+        "prefer-rest-params": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+
+        "@typescript-eslint/no-unused-vars": ["error", {
+            argsIgnorePattern: "^_.*",
+            varsIgnorePattern: "^_.*",
+            destructuredArrayIgnorePattern: "^_.*",
+        }],
+
+        "react/display-name": "off",
+
+        "react/function-component-definition": ["error", {
+            namedComponents: "arrow-function",
+            unnamedComponents: "arrow-function",
+        }],
+
+        "react/no-danger": "error",
+        "jsx-a11y/no-static-element-interactions": "off",
+        "jsx-a11y/click-events-have-key-events": "off",
+    },
+}];
