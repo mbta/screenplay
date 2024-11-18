ee121e1ca29c399e6a4ac3a2c81ccfa07c9e1e33
cmaddox5 <christian.maddox93@gmail.com>
2024-11-18 11:26:29 -0500

    Allow message to appear in tab navigation.

--- assets/js/components/Dashboard/PaMessagesPage.tsx
+++ assets/js/components/Dashboard/PaMessagesPage.tsx
@@ -345,7 +345,14 @@ const PaMessageRow: ComponentType<PaMessageRowProps> = ({
 
   return (
     <tr onClick={() => navigate(`/pa-messages/${paMessage.id}/edit`)}>
-      <td className="pa-message-table__message">{paMessage.visual_text}</td>
+      <td className="pa-message-table__message">
+        <a
+          href={`/pa-messages/${paMessage.id}/edit`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          {paMessage.visual_text}
+        </a>
+      </td>
       <td className="pa-message-table__interval">
         {paMessage.interval_in_minutes} min
       </td>
