8991223f0cf61f6506bffd8f4de115d2c7f4bc91
Dean <deanshi@users.noreply.github.com>
2025-10-06 13:59:17 -0400

    Add audio_url for PA Message datamodel (#656)
    - Added optional `audio_url` to `pa_message` and `static_template` to begin supporting the field
    - Modified `audio_text` to be optional and add some null check logic to pass empty strings
    - Added table migration to add `audio_url` and make `audio_text` optional

---
+++ priv/repo/migrations/20250929202506_add_audio_url_column.exs
@@ -0,0 +1,12 @@
+defmodule Screenplay.Repo.Migrations.AddAudioUrlColumn do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  def change do
+    alter table("pa_message") do
+      add :audio_url, :text
+      modify :audio_text, :text, null: true
+    end
+  end
+end
