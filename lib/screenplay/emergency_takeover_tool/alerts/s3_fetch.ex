c1a3d6f010023b38cd2330bb60f1327a7d817630
robbie.sundstrom <rsundstrom@mbta.com>
2026-05-22 17:26:14 -0400

    better error handling on S3 uploads/deletions

--- lib/screenplay/emergency_takeover_tool/alerts/s3_fetch.ex
+++ lib/screenplay/emergency_takeover_tool/alerts/s3_fetch.ex
@@ -18,14 +18,22 @@ defmodule Screenplay.EmergencyTakeoverTool.Alerts.S3Fetch do
   def upload_takeover_image(alert_id, image_data, image_type) do
     image_path = "#{asset_directory()}#{alert_id}/#{image_type}.png"
 
-    %{status_code: 200} =
-      ExAws.S3.put_object(bucket(), image_path, image_data,
-        content_type: "image/png",
-        acl: :public_read
-      )
-      |> ExAws.request!()
+    case ExAws.S3.put_object(bucket(), image_path, image_data,
+           content_type: "image/png",
+           acl: :public_read
+         )
+         |> ExAws.request() do
+      {:ok, %{status_code: 200}} ->
+        :ok
 
-    :ok
+      {:ok, response} ->
+        Logger.error("Error uploading takeover image to S3: #{inspect(response)}")
+        :error
+
+      {:error, reason} ->
+        Logger.error("Error uploading takeover image to S3: #{inspect(reason)}")
+        :error
+    end
   end
 
   @spec delete_takeover_images(String.t()) :: :ok
@@ -40,13 +48,14 @@ defmodule Screenplay.EmergencyTakeoverTool.Alerts.S3Fetch do
       {:ok, %{deleted: _, errors: []}} ->
         :ok
 
+      # Even on errors, we don't want to crash the process since deletion isn't essential.
       {:ok, %{deleted: _, errors: errors}} ->
         Logger.error("Error deleting images from S3: #{inspect(errors)}")
-        :error
+        :ok
 
       {:error, reason} ->
         Logger.error("Error deleting images from S3: #{inspect(reason)}")
-        :error
+        :ok
     end
   end
 
