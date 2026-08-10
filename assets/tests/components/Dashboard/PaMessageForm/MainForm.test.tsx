e2f7ae126ca235ad25229658ccafeadad4f5ed39
Brett Heath-Wlaz <bheathwlaz@mbta.com>
2026-08-10 11:30:36 -0400

    expand PA message character validation (#797)

--- assets/tests/components/Dashboard/PaMessageForm/MainForm.test.tsx
+++ assets/tests/components/Dashboard/PaMessageForm/MainForm.test.tsx
@@ -4,9 +4,7 @@ describe("MainForm", () => {
   describe("getInvalidVisualText()", () => {
     test.each([
       { input: "hyphen - endash – emdash —", expected: new Set("–—") },
-      { input: "See the train coming?", expected: new Set("?") },
-      { input: "Green Line[D] service suspended", expected: new Set("[]") },
-      { input: "Service will result in ~15 minutes", expected: new Set("~") },
+      { input: "!@#$%^&*()?€©", expected: new Set("€©") },
     ])("detects invalid text", ({ input, expected }) => {
       expect(getInvalidVisualText(input)).toEqual(expected);
     });
